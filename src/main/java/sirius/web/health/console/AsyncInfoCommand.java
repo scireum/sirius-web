41441319afadfa43d63749a1d445e42493ffc6af
aha <aha@scireum.de>
2015-04-29 23:46:01 +0200

    Added background tasks and operations to the async command

--- src/main/java/sirius/web/health/console/AsyncInfoCommand.java
+++ src/main/java/sirius/web/health/console/AsyncInfoCommand.java
@@ -10,9 +10,11 @@ package sirius.web.health.console;
 
 import sirius.kernel.async.Async;
 import sirius.kernel.async.AsyncExecutor;
+import sirius.kernel.async.Operation;
 import sirius.kernel.di.std.Register;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 /**
  * Console command which reports statistics for all known executors.
@@ -25,7 +27,14 @@ public class AsyncInfoCommand implements Command {
 
     @Override
     public void execute(Output output, String... params) throws Exception {
-        output.apply("%-20s %8s %8s %8s %12s %8s %8s", "POOL", "ACTIVE", "QUEUED", "TOTAL", "DURATION", "BLOCKED", "DROPPED");
+        output.apply("%-20s %8s %8s %8s %12s %8s %8s",
+                     "POOL",
+                     "ACTIVE",
+                     "QUEUED",
+                     "TOTAL",
+                     "DURATION",
+                     "BLOCKED",
+                     "DROPPED");
         output.separator();
         for (AsyncExecutor exec : Async.getExecutors()) {
             output.apply("%-20s %8d %8d %8d %12.1f %8d %8d",
@@ -38,6 +47,23 @@ public class AsyncInfoCommand implements Command {
                          exec.getDropped());
         }
         output.separator();
+        output.blankLine();
+        output.apply("Background Queues");
+        output.separator();
+        for (String worker : Async.getBackgroundWorkers()) {
+            output.line(worker);
+        }
+        output.separator();
+        List<Operation> ops = Operation.getActiveOperations();
+        if (!ops.isEmpty()) {
+            output.blankLine();
+            output.apply("Active Operations");
+            output.separator();
+            for (Operation op : ops) {
+                output.line(op.toString());
+            }
+            output.separator();
+        }
     }
 
     @Override
