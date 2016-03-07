19bd8efa7d9bb555f474483f262744bfe8dc2a9b
aha <aha@scireum.de>
2016-03-07 15:17:33 +0100

    Added to possibilit to lock a scope as "in maintenance".
    Scopes can directly or via an AdapterFactory provide a MaintenanceInfo
    which is used by the template.html to then show a "lock" message.

--- src/main/java/sirius/web/security/MaintenanceInfo.java
+++ src/main/java/sirius/web/security/MaintenanceInfo.java
@@ -14,6 +14,10 @@ import javax.annotation.Nullable;
 
 /**
  * A {@link ScopeInfo} can additionally implement this interface to signal the current maintenance status of a scope.
+ * <p>
+ * To keep the {@link ScopeInfo} class small and conceise, this interface is obtained via the <tt>Adapters</tt>
+ * framework by calling {@link ScopeInfo#tryAs(Class)}. Therefore either a subclass implementing this interface
+ * has to be used or an appropriate {@link sirius.kernel.di.morphium.AdapterFactory} has to be created.
  */
 public interface MaintenanceInfo {
 
