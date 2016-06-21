4ce2237d7f960abc0321ad0415f74201f65bdab4
aha <aha@scireum.de>
2016-06-21 21:59:15 +0200

    Made Permission and NotPermission repeatable

--- src/main/java/sirius/web/security/Permission.java
+++ src/main/java/sirius/web/security/Permission.java
@@ -9,6 +9,7 @@
 package sirius.web.security;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -27,6 +28,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
+@Repeatable(PermissionList.class)
 public @interface Permission {
     /**
      * Names the permission which has to be presnet for the current user in order to execute the annotated element
