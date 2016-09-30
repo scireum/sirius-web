e1bc903945499699b0f78736ee18823809bd24f8
aha <aha@scireum.de>
2016-09-30 11:15:11 +0200

    Typo-Fixx

--- src/main/java/sirius/web/http/Response.java
+++ src/main/java/sirius/web/http/Response.java
@@ -1069,7 +1069,7 @@ public class Response {
      * Converts a string into a ByteBuf
      */
     private ByteBuf wrapUTF8String(String content) {
-        // Returns a heap buffer - but strings are almost always compressed (HTML templtes etc.) so this
+        // Returns a heap buffer - but strings are almost always compressed (HTML templates etc.) so this
         // is probably faster
         return Unpooled.copiedBuffer(content.toCharArray(), Charsets.UTF_8);
     }
