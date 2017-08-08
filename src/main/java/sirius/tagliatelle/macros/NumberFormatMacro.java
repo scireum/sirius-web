2984424f58f2f8e9cfd74c988b43cc1d21471192
mbp-aru <aru@mbp-aru.scireum.local>
2017-08-08 10:22:26 +0200

    works for all kinds of Numbers now and is constant

--- src/main/java/sirius/tagliatelle/macros/NumberFormatMacro.java
+++ src/main/java/sirius/tagliatelle/macros/NumberFormatMacro.java
@@ -18,7 +18,7 @@ import java.text.DecimalFormat;
 import java.util.List;
 
 /**
- * Uses DecimalFormatter to format large ints or longs with dots for better readability.
+ * Uses DecimalFormatter to format large numbers with dots for better readability.
  */
 @Register
 public class NumberFormatMacro implements Macro {
@@ -30,10 +30,8 @@ public class NumberFormatMacro implements Macro {
 
     @Override
     public void verifyArguments(List<Expression> args) {
-        if (args.size() != 1
-            || !Tagliatelle.isAssignableTo(args.get(0).getType(), long.class)
-            || !Tagliatelle.isAssignableTo(args.get(0).getType(), int.class)) {
-            throw new IllegalArgumentException("Expected the parameter to be of primitive long or int type.");
+        if (args.size() != 1 || !Tagliatelle.isAssignableTo(args.get(0).getType(), Number.class)) {
+            throw new IllegalArgumentException("Expected the parameter to be of a Number type.");
         }
     }
 
@@ -42,12 +40,12 @@ public class NumberFormatMacro implements Macro {
         DecimalFormat formatter = new DecimalFormat();
         formatter.setGroupingUsed(true);
         formatter.setMaximumFractionDigits(2);
-        return formatter.format((long) args[0].eval(ctx));
+        return formatter.format(args[0].eval(ctx));
     }
 
     @Override
     public boolean isConstant(Expression[] args) {
-        return false;
+        return true;
     }
 
     @Override
