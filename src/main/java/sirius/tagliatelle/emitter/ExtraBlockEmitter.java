b65663f0cd09fe84b6ade06b4ca9ad312879dc5d
aha <aha@scireum.de>
2017-09-06 10:14:27 +0200

    Permits to create additional output blocks when rendering a template.
    By defining <i:block> sections at the top level of a template,
    additional outputs can be created while rendering the template.
    These can be acquired by invoking GlobalRenderContext.getExtraBlock()

---
+++ src/main/java/sirius/tagliatelle/emitter/ExtraBlockEmitter.java
@@ -0,0 +1,70 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.tagliatelle.emitter;
+
+import parsii.tokenizer.Position;
+import sirius.tagliatelle.expression.ExpressionVisitor;
+import sirius.tagliatelle.rendering.LocalRenderContext;
+
+import javax.annotation.Nonnull;
+import java.util.function.Function;
+
+/**
+ * Emits the contents of an i:block defined at top-level into an extra block output of the context.
+ * <p>
+ * This permits to render several strings and sections within a single template.
+ *
+ * @see sirius.tagliatelle.rendering.GlobalRenderContext#getExtraBlock(String)
+ */
+public class ExtraBlockEmitter extends Emitter {
+
+    private final String name;
+    private Emitter body;
+
+    public ExtraBlockEmitter(String name, Emitter body) {
+        super(body.startOfBlock);
+        this.name = name;
+        this.body = body;
+    }
+
+    @Override
+    protected void emitToContext(@Nonnull LocalRenderContext context) throws Exception {
+        context.getGlobalContext().beginExtraBlock();
+        try {
+            body.emit(context);
+        } finally {
+            context.getGlobalContext().completeExtraBlock(name);
+        }
+    }
+
+    @Nonnull
+    @Override
+    public Emitter copy() {
+        return new ExtraBlockEmitter(name, body.copy());
+    }
+
+    @Nonnull
+    @Override
+    public Emitter reduce() {
+        body = body.reduce();
+        return this;
+    }
+
+    @Nonnull
+    @Override
+    public Emitter propagateVisitor(@Nonnull EmitterVisitor visitor) {
+        body = body.propagateVisitor(visitor);
+        return visitor.visitThis(this);
+    }
+
+    @Override
+    public void visitExpressions(@Nonnull Function<Position, ExpressionVisitor> visitor) {
+        body.visitExpressions(visitor);
+    }
+}
