5b882e83c90372ff015e16730eb94623b3d7ebeb
Dennis Heller <dhe@scireum.de>
2017-12-15 12:12:05 +0100

    Creates a toList-macro
    (cherry picked from commit 5ac4d62)

---
+++ src/main/java/sirius/tagliatelle/macros/ToListMacro.java
@@ -0,0 +1,64 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.tagliatelle.macros;
+
+import sirius.kernel.di.std.Register;
+import sirius.tagliatelle.expression.Expression;
+import sirius.tagliatelle.rendering.LocalRenderContext;
+
+import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Represents a list containing all given parameters. If only one parameter of an array type is given, it will be
+ * expanded into the list instead.
+ */
+@Register
+public class ToListMacro implements Macro {
+
+    @Override
+    public Class<?> getType() {
+        return List.class;
+    }
+
+    @Override
+    public void verifyArguments(List<Expression> args) {
+        if (args.isEmpty()) {
+            throw new IllegalArgumentException(
+                    "Expected at least one parameter");
+        }
+    }
+
+    @Override
+    public Object eval(LocalRenderContext ctx, Expression[] args) {
+        if (args.length == 1 && args[0].getType().isArray()) {
+            return Arrays.stream((Object[]) args[0].eval(ctx)).collect(Collectors.toList());
+        } else {
+            return Arrays.stream(args).map(element -> element.eval(ctx)).collect(Collectors.toList());
+        }
+    }
+
+    @Override
+    public boolean isConstant(Expression[] args) {
+        return true;
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "toList";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Returns a list containing of the given items or the given array";
+    }
+}
