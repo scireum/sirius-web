b4bbb934eb349a525b7e179c8cc625766dc0c7cf
aha <aha@scireum.de>
2018-02-05 23:10:44 +0100

    Permits to render the contents of a block into a string and to store the contents of the i:define tag into a local variable.

---
+++ src/main/java/sirius/tagliatelle/macros/RenderBlockMacro.java
@@ -0,0 +1,69 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.tagliatelle.macros;
+
+import sirius.kernel.di.std.Register;
+import sirius.tagliatelle.Tagliatelle;
+import sirius.tagliatelle.emitter.InlineTemplateEmitter;
+import sirius.tagliatelle.expression.Expression;
+import sirius.tagliatelle.expression.RenderEmitterExpression;
+import sirius.tagliatelle.rendering.GlobalRenderContext;
+import sirius.tagliatelle.rendering.LocalRenderContext;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Returns the contents of the given block as string.
+ */
+@Register
+public class RenderBlockMacro implements Macro {
+
+    @Override
+    public Class<?> getType() {
+        return String.class;
+    }
+
+    @Override
+    public void verifyArguments(List<Expression> args) {
+        if (args.size() != 1 || !Tagliatelle.isAssignableTo(args.get(0).getType(), String.class) || !args.get(0)
+                                                                                                         .isConstant()) {
+            throw new IllegalArgumentException("Expected a single String as argument.");
+        }
+    }
+
+    @Override
+    public Object eval(LocalRenderContext ctx, Expression[] args) {
+        return ctx.getGlobalContext().emitToString(() -> {
+            ctx.emitBlock((String) args[0].eval(null));
+        }, GlobalRenderContext::escapeRAW);
+    }
+
+    @Override
+    public boolean isConstant(Expression[] args) {
+        return false;
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "renderToString";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Renders the block with the given name into a string.";
+    }
+
+    @Override
+    public Expression dereference(Function<String, InlineTemplateEmitter> blocks, Expression[] args) {
+        return new RenderEmitterExpression(blocks.apply((String) args[0].eval(null)));
+    }
+}
