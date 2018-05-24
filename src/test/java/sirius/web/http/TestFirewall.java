128a9cf6f233e75c986e5a2c47c6052aa4957beb
aha <aha@scireum.de>
2018-05-24 11:26:44 +0200

    Provides a simple unit-test for the firewall interface.

---
+++ src/test/java/sirius/web/http/TestFirewall.java
@@ -0,0 +1,33 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.web.http;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import sirius.kernel.di.std.Register;
+
+@Register
+public class TestFirewall implements Firewall {
+
+    public static boolean blockAllIPs = false;
+
+    @Override
+    public boolean isIPBlacklisted(WebContext ctx) {
+        return blockAllIPs;
+    }
+
+    @Override
+    public boolean handleRateLimiting(WebContext ctx, String realm) {
+        if ("blocked".equals(realm)) {
+            ctx.respondWith().error(HttpResponseStatus.TOO_MANY_REQUESTS);
+            return true;
+        }
+
+        return false;
+    }
+}
