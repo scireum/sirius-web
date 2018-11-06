5a0d8f91ae89bc14b43711dd33884236a2a9f13c
meg <meg@scireum.de>
2018-11-06 14:02:14 +0100

    Provides instead of setting languages from the Web/UserContext to the CallContext
    so already set languages will not be overridden

--- src/main/java/sirius/web/http/WebServerHandler.java
+++ src/main/java/sirius/web/http/WebServerHandler.java
@@ -175,7 +175,7 @@ class WebServerHandler extends ChannelDuplexHandler implements ActiveHTTPConnect
         // if everything else fails, parse the lang header.
         currentCall.deferredSetLang(callContext -> {
             if (!callContext.get(UserContext.class).bindUserIfPresent(wc).isPresent()) {
-                callContext.setLang(NLS.makeLang(wc.getLang()));
+                callContext.provideLang(NLS.makeLang(wc.getLang()));
             }
         });
 
