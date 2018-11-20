7fc6cb9c22cf224adcacea15cea56d0a57deeece
Dennis Heller <dhe@scireum.de>
2018-11-20 14:27:07 +0100

    Creates a tagliatelle template for time inputs

---
+++ src/main/java/sirius/tagliatelle/macros/ToTimeInputValueMacro.java
@@ -0,0 +1,63 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.tagliatelle.macros;
+
+import sirius.kernel.di.std.Register;
+import sirius.tagliatelle.Tagliatelle;
+import sirius.tagliatelle.expression.Expression;
+import sirius.tagliatelle.rendering.LocalRenderContext;
+
+import javax.annotation.Nonnull;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.Temporal;
+import java.util.List;
+
+/**
+ * Formats the given parameter as a value string to be inserted into an {@code <input type="time">}.
+ */
+@Register
+public class ToTimeInputValueMacro implements Macro {
+    @Override
+    public Class<?> getType() {
+        return String.class;
+    }
+
+    @Override
+    public void verifyArguments(List<Expression> args) {
+        if (args.size() != 1 || !Tagliatelle.isAssignableTo(args.get(0).getType(), Temporal.class)) {
+            throw new IllegalArgumentException("One parameter of type Temporal is expected.");
+        }
+    }
+
+    @Override
+    public Object eval(LocalRenderContext ctx, Expression[] args) {
+        Object temporal = args[0].eval(ctx);
+        if (temporal == null) {
+            return "";
+        } else {
+            return DateTimeFormatter.ISO_TIME.format((Temporal) temporal);
+        }
+    }
+
+    @Override
+    public boolean isConstant(Expression[] args) {
+        return true;
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "toTimeInputValue";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Formats the given parameter as a value string to be inserted into an <input type=\"time\">.";
+    }
+}
