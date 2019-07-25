ab328ff0c29b685131919d6ed82ce54af02b2041
aha <aha@scireum.de>
2019-07-25 17:28:02 +0200

    Adds a test lifecycle which ensures that all visible templates can be compiled.
    Templates which contain intentional bugs / errors are removed from this check by
    adding a pragma "unchecked".

---
+++ src/test/java/sirius/tagliatelle/ReportBrokenTemplates.java
@@ -0,0 +1,61 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.tagliatelle;
+
+import sirius.kernel.Sirius;
+import sirius.kernel.TestLifecycleParticipant;
+import sirius.kernel.di.std.Part;
+import sirius.kernel.di.std.Register;
+import sirius.kernel.health.Exceptions;
+import sirius.tagliatelle.compiler.CompilationContext;
+import sirius.tagliatelle.compiler.CompileException;
+import sirius.tagliatelle.compiler.Compiler;
+import sirius.web.resources.Resource;
+import sirius.web.resources.Resources;
+
+import java.util.regex.Pattern;
+
+@Register
+public class ReportBrokenTemplates implements TestLifecycleParticipant {
+
+    @Part
+    private Tagliatelle engine;
+
+    @Part
+    private Resources resources;
+
+    @Override
+    public void beforeTests() {
+
+    }
+
+    @Override
+    public void afterTests() {
+        Sirius.getClasspath().find(Pattern.compile(".*.pasta")).forEach(m -> {
+            String templateName = "/" + m.group(0);
+            if (templateName.startsWith("/default/")) {
+                templateName = templateName.substring(8);
+            }
+            Resource resource = resources.resolve(templateName).orElse(null);
+            CompilationContext compilationContext = engine.createCompilationContext(templateName, resource, null);
+            try {
+                Compiler compiler = new Compiler(compilationContext, resource.getContentAsString());
+                compiler.compile();
+            } catch (CompileException e) {
+                if (!compilationContext.getTemplate().getPragma("unchecked").asBoolean()) {
+                    throw Exceptions.handle()
+                                    .withSystemErrorMessage("Failed to compile: %s - %s",
+                                                            templateName,
+                                                            e.getErrors().get(0).toString())
+                                    .handle();
+                }
+            }
+        });
+    }
+}
