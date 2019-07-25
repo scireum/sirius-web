f677ea0c169aa52c067c65fa0422c7b443052ef8
Jan Müller <jmu@scireum.de>
2019-07-25 17:50:45 +0200

    Properly match the taglib names for listing in the help system

--- src/main/java/sirius/tagliatelle/Tagliatelle.java
+++ src/main/java/sirius/tagliatelle/Tagliatelle.java
@@ -93,7 +93,7 @@ public class Tagliatelle {
         if (taglibTags == null) {
             MultiMap<String, String> result = MultiMap.createOrdered();
             Sirius.getClasspath()
-                  .find(Pattern.compile("(default/|customizations/[^/]+/)?taglib/([a-z]+)/(.*).pasta"))
+                  .find(Pattern.compile("(default/|customizations/[^/]+/)?taglib/([a-z]+)/([^.]*).*.pasta"))
                   .forEach(m -> result.put(m.group(2), m.group(3)));
             taglibTags = result;
         }
