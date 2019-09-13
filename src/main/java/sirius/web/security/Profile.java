89670eb82f589cc19eb7d9a982d3c96eae38f415
Sascha Bieberstein <sabieber@users.noreply.github.com>
2019-09-13 10:58:33 +0200

    JavaDoc
    Co-Authored-By: Andreas Haufler <aha@scireum.de>

--- src/main/java/sirius/web/security/Profile.java
+++ src/main/java/sirius/web/security/Profile.java
@@ -61,7 +61,7 @@ class Profile {
     /**
      * Validates this profile and throws exception if problems exist.
      * <p>
-     * An exception will be thrown if the profile refers to another profile applied earlier than itself.
+     * @throws HandledException if the profile refers to another profile applied earlier than itself.
      */
     public void validate() {
         Extension thisProfile = Sirius.getSettings().getExtension(SECURITY_PROFILES, name);
