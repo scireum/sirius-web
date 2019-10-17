3772d8c66b22a62e3371650b0d5ec8a9b003a0fe
aha <aha@scireum.de>
2019-10-17 15:53:52 +0200

    Always report an error when detecting deprecated code.

--- src/main/java/sirius/tagliatelle/compiler/CompilationContext.java
+++ src/main/java/sirius/tagliatelle/compiler/CompilationContext.java
@@ -412,27 +412,16 @@ public class CompilationContext {
 
     private void outputArgumentDeprecationWarning(Position position, TemplateArgument arg) {
         if (arg.getDeprecationWarning() != null) {
-            if (this.parent == null && (Sirius.isDev() || Sirius.isStartedAsTest())) {
-                error(position, "The attribute '%s' is deprecated: %s", arg.getName(), arg.getDeprecationWarning());
-            } else {
-                warning(position, "The attribute '%s' is deprecated: %s", arg.getName(), arg.getDeprecationWarning());
-            }
+            warning(position, "The attribute '%s' is deprecated: %s", arg.getName(), arg.getDeprecationWarning());
         }
     }
 
     private void outputTemplateDeprecationWarning(Position position, Template template) {
         if (Strings.isFilled(template.getPragma(PRAGMA_DEPRECATED))) {
-            if (this.parent == null && (Sirius.isDev() || Sirius.isStartedAsTest())) {
-                error(position,
-                      "The template '%s' is deprecated: %s",
-                      template.getShortName(),
-                      template.getPragma(PRAGMA_DEPRECATED));
-            } else {
-                warning(position,
-                        "The template '%s' is deprecated: %s",
-                        template.getShortName(),
-                        template.getPragma(PRAGMA_DEPRECATED));
-            }
+            warning(position,
+                    "The template '%s' is deprecated: %s",
+                    template.getShortName(),
+                    template.getPragma(PRAGMA_DEPRECATED));
         }
     }
 
