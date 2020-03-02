3962f41142da5a4071114588519bd4870b2147c1
Marc Egenrieder <meg@scireum.de>
2020-03-02 10:00:51 +0100

    Adds a template macro to get the current unixtime in days
    Fixes: MIO-5275

---
+++ src/main/java/sirius/tagliatelle/macros/UnixtimeInDaysMacro.java
@@ -0,0 +1,58 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.tagliatelle.macros;
+
+import parsii.tokenizer.Position;
+import sirius.kernel.di.std.Register;
+import sirius.tagliatelle.compiler.CompilationContext;
+import sirius.tagliatelle.expression.Expression;
+import sirius.tagliatelle.rendering.LocalRenderContext;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+/**
+ * Gets the current time since 1970 in days
+ */
+@Register
+public class UnixtimeInDaysMacro implements Macro {
+
+    @Override
+    public Class<?> getType() {
+        return String.class;
+    }
+
+    @Override
+    public void verifyArguments(CompilationContext context, Position position, List<Expression> args) {
+        if (!args.isEmpty()) {
+            throw new IllegalArgumentException("'unixtimeInDays' does not take arguments.");
+        }
+    }
+
+    @Override
+    public Object eval(LocalRenderContext ctx, Expression[] args) {
+        return System.currentTimeMillis() / 1000 / 86400;
+    }
+
+    @Override
+    public boolean isConstant(Expression[] args) {
+        return false;
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "unixtimeInDays";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Gets the current time since 1970 in days";
+    }
+}
