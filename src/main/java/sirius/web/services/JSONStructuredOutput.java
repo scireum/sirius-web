ac50291d47e3805e4eb97ef50b477ab36fff5e63
aha <aha@scireum.de>
2020-07-03 11:29:06 +0200

    Permits to properly output embedded JSON.
    If a property contains a JSONObject or
    JSONArray, we properly transform this into
    proper JSON within this output.

--- src/main/java/sirius/web/services/JSONStructuredOutput.java
+++ src/main/java/sirius/web/services/JSONStructuredOutput.java
@@ -8,6 +8,8 @@
 
 package sirius.web.services;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import sirius.kernel.commons.Strings;
 import sirius.kernel.health.Exceptions;
 import sirius.kernel.health.HandledException;
@@ -227,6 +229,20 @@ public class JSONStructuredOutput extends AbstractStructuredOutput {
 
     @Override
     public void writeProperty(String name, Object data) {
+        if (data instanceof JSONObject) {
+            beginObject(name);
+            ((JSONObject) data).forEach(this::property);
+            endObject();
+        } else if (data instanceof JSONArray) {
+            beginArray(name);
+            ((JSONArray) data).forEach(element -> property("", element));
+            endArray();
+        } else {
+            writePlainProperty(name, data);
+        }
+    }
+
+    private void writePlainProperty(String name, Object data) {
         try {
             addRequiredComma();
             if (getCurrentType() == ElementType.OBJECT) {
