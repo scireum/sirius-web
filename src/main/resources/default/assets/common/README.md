fc3f58dc1701aa79649e142a9c9bd456e6c8aa7e
aha <aha@scireum.de>
2020-10-22 09:34:04 +0200

    Provides the initial setup of the sirius common JavaScript library.
    This will be used to aggregate helpers and utilities for JS.

---
+++ src/main/resources/default/assets/common/README.md
@@ -0,0 +1,13 @@
+# Common JavaScript Library
+
+Provides a helper library providing commonly used JS snippets. Use or include
+[common.js.pasta](common.js.pasta) which brings all other parts with it.
+
+Additionally, for IE include [ie-polyfill.js](ie-polyfill.js.pasta) which brings
+some libraries otherwise missing from Internet Explorer.
+
+Note that the library is internally split up into several files to maintain some
+kind of overview and readability.
+
+Note that all functionality provided by this are wrapped in the **sirius** object
+to avoid any namespace clashed.
---
+++ src/main/resources/default/assets/common/polyfill.js
@@ -0,0 +1,74 @@
+/**
+ * polyfill for the date.now method
+ * see: https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Date/now
+ */
+if (!Date.now) {
+    Date.now = function now() {
+        return new Date().getTime();
+    };
+}
+
+/**
+ * polyfill for the array.includes method
+ * see https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+ */
+if (!Array.prototype.includes) {
+    Array.prototype.includes = function (searchElement /*, fromIndex*/) {
+        'use strict';
+        if (this == null) {
+            throw new TypeError('Array.prototype.includes called on null or undefined');
+        }
+
+        var O = Object(this);
+        var len = parseInt(O.length, 10) || 0;
+        if (len === 0) {
+            return false;
+        }
+        var n = parseInt(arguments[1], 10) || 0;
+        var k;
+        if (n >= 0) {
+            k = n;
+        } else {
+            k = len + n;
+            if (k < 0) {
+                k = 0;
+            }
+        }
+        var currentElement;
+        while (k < len) {
+            currentElement = O[k];
+            if (searchElement === currentElement ||
+                (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+                return true;
+            }
+            k++;
+        }
+        return false;
+    };
+}
+/**
+ * Polyfill for the String.startsWith function
+ * see https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith#Polyfill
+ */
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position) {
+        position = position || 0;
+        return this.indexOf(searchString, position) === position;
+    };
+}
+
+/**
+ * Polyfill for the String.endsWith function
+ * see https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+ */
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function(searchString, position) {
+        var subjectString = this.toString();
+        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+            position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.indexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+    };
+}
