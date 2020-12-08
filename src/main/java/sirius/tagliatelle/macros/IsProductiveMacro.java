9e4829037e6b916af8d3d4179beaa4294bd48485
Marvin Woop <mwo@scireum.de>
2020-12-08 14:53:22 +0100

    Changed env mode macro classes to new macro base class.

--- src/main/java/sirius/tagliatelle/macros/IsProductiveMacro.java
+++ src/main/java/sirius/tagliatelle/macros/IsProductiveMacro.java
@@ -8,41 +8,22 @@
 
 package sirius.tagliatelle.macros;
 
-import parsii.tokenizer.Position;
 import sirius.kernel.Sirius;
 import sirius.kernel.di.std.Register;
-import sirius.tagliatelle.compiler.CompilationContext;
 import sirius.tagliatelle.expression.Expression;
 import sirius.tagliatelle.rendering.LocalRenderContext;
 
 import javax.annotation.Nonnull;
-import java.util.List;
 
 /**
  * This macro determines if the current system environment is in production {@link sirius.kernel.Setup.Mode}.
  */
 @Register
-public class IsProductiveMacro implements Macro {
-    @Override
-    public Class<?> getType() {
-        return boolean.class;
-    }
-
-    @Override
-    public void verifyArguments(CompilationContext context, Position pos, List<Expression> args) {
-        if (!args.isEmpty()) {
-            throw new IllegalArgumentException("No arguments expected!");
-        }
-    }
+public class IsProductiveMacro extends EnvironmentModeMacro {
 
     @Override
     public Object eval(LocalRenderContext ctx, Expression[] args) {
         return Sirius.isProd();
-    }
-
-    @Override
-    public boolean isConstant(Expression[] args) {
-        return false;
     }
 
     @Override
--- src/main/java/sirius/tagliatelle/macros/IsStagingMacro.java
+++ src/main/java/sirius/tagliatelle/macros/IsStagingMacro.java
@@ -8,41 +8,22 @@
 
 package sirius.tagliatelle.macros;
 
-import parsii.tokenizer.Position;
 import sirius.kernel.Sirius;
 import sirius.kernel.di.std.Register;
-import sirius.tagliatelle.compiler.CompilationContext;
 import sirius.tagliatelle.expression.Expression;
 import sirius.tagliatelle.rendering.LocalRenderContext;
 
 import javax.annotation.Nonnull;
-import java.util.List;
 
 /**
  * This macro determines if the current system environment is in staging {@link sirius.kernel.Setup.Mode}.
  */
 @Register
-public class IsStagingMacro implements Macro {
-    @Override
-    public Class<?> getType() {
-        return boolean.class;
-    }
-
-    @Override
-    public void verifyArguments(CompilationContext context, Position pos, List<Expression> args) {
-        if (!args.isEmpty()) {
-            throw new IllegalArgumentException("No arguments expected!");
-        }
-    }
+public class IsStagingMacro extends EnvironmentModeMacro {
 
     @Override
     public Object eval(LocalRenderContext ctx, Expression[] args) {
         return Sirius.isStaging();
-    }
-
-    @Override
-    public boolean isConstant(Expression[] args) {
-        return false;
     }
 
     @Override
