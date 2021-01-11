3b41d81959f362b620cea4b4c135d233e0590710
aha <aha@scireum.de>
2021-01-11 17:33:03 +0100

    Permits to supply optional class aliases.
    This is mainly used by sirius biz which automatically provides an alias for each helper
    and entity. However, if an application already uses an alias, we ignore the entity
    or helper.

--- src/main/java/sirius/pasta/noodle/ClassAliasProvider.java
+++ src/main/java/sirius/pasta/noodle/ClassAliasProvider.java
@@ -8,6 +8,8 @@
 
 package sirius.pasta.noodle;
 
+import sirius.kernel.di.std.Priorized;
+
 import java.util.function.BiConsumer;
 
 /**
@@ -16,11 +18,20 @@ import java.util.function.BiConsumer;
  * Implementations can be {@link sirius.kernel.di.std.Register registered} and then provide aliases for commonly used
  * classes.
  */
-public interface ClassAliasProvider {
+public interface ClassAliasProvider extends Priorized {
     /**
      * Collects all available aliases and their target classes
      *
      * @param consumer the consumer to supply with aliases
      */
     void collectAliases(BiConsumer<String, Class<?>> consumer);
+
+    /**
+     * Permits to provide optional aliases which should only be introduced if no conflicting alias exists.
+     *
+     * @param consumer the consumer to supply with aliases
+     */
+    default void collectOptionalAliases(BiConsumer<String, Class<?>> consumer) {
+    }
+
 }
