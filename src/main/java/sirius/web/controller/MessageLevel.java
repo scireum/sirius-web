47895a6f89f2a254b8e40e41b72e0dfe3d82d8a5
aha <aha@scireum.de>
2021-02-22 14:40:14 +0100

    Adds some infos required by tycho.

--- src/main/java/sirius/web/controller/MessageLevel.java
+++ src/main/java/sirius/web/controller/MessageLevel.java
@@ -9,35 +9,48 @@ public enum MessageLevel {
     /**
      * Declares a message as success.
      */
-    SUCCESS("alert-success"),
+    SUCCESS("alert-success", "green"),
 
     /**
      * Declares a message as information.
      */
-    INFO("alert-info"),
+    INFO("alert-info", "blue"),
 
     /**
      * Declares a message as warning.
      */
-    WARNING("alert-warning"),
+    WARNING("alert-warning", "yellow"),
 
     /**
      * Declares a message as error.
      */
-    PROBLEM("alert-danger");
+    PROBLEM("alert-danger", "red");
 
     private final String cssClass;
+    private final String color;
 
-    MessageLevel(String cssClass) {
+    MessageLevel(String cssClass, String color) {
         this.cssClass = cssClass;
+        this.color = color;
     }
 
     /**
      * Returns the Bootstrap CSS class used to render the message.
      *
      * @return the name of the css class used to render the message
+     * @deprecated Used by Wonderger. Tycho will use getColor.
      */
+    @Deprecated
     public String getCssClass() {
         return cssClass;
+    }
+
+    /**
+     * Returns the color used to represent this message level.
+     *
+     * @return the name of the color used for this message level
+     */
+    public String getColor() {
+        return color;
     }
 }
