151e654d04b68340044924ab272a50304d2ac712
Jan Müller <jmu@scireum.de>
2021-04-08 12:03:59 +0200

    Support resolving the return types of the whole inheritance tree
    In order to be able to resolve the types from methods that are defined
    in super classes or interfaces, we need to also add their type
    parameters. This is done recursively to support more than one level of
    inheritance.
    
    This added the requirement to scope the type parameters in the type
    table, as (like in our test) the same letter might be used multiple
    times with different types.
    
    Fixes: SIRI-346

--- src/main/java/sirius/pasta/noodle/compiler/TypeTools.java
+++ src/main/java/sirius/pasta/noodle/compiler/TypeTools.java
@@ -10,7 +10,10 @@ package sirius.pasta.noodle.compiler;
 
 import sirius.pasta.noodle.compiler.ir.Node;
 
+import javax.annotation.Nonnull;
+import java.lang.reflect.Executable;
 import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.GenericDeclaration;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
@@ -24,30 +27,95 @@ import java.util.Map;
  */
 public class TypeTools {
 
+    /**
+     * A table to store type variables and their types.
+     * <p>
+     * The type variable names are stored scoped, i.e. <tt>java.lang.List#E</tt>, to prevent collisions when the same
+     * letter is used multiple times in the inheritance tree.
+     */
     private final Map<String, Type> typeTable = new LinkedHashMap<>();
 
     /**
      * Computes a type table for the given class.
+     * <p>
+     * This will first build up a type table for all type parameters for the whole inheritance tree. The resulting table
+     * might look like this:
+     * <table>
+     * <tr><td>java.lang.List#E</td><td>String</td></tr>
+     * <tr><td>java.lang.Collection#E</td><td>java.lang.List#E</td></tr>
+     * <tr><td>java.lang.Iterable#T</td><td>java.lang.Collection#E</td></tr>
+     * </table>
+     * <p>
+     * This table is then reduced by replacing the known type parameters, which results in:
+     * <table>
+     * <tr><td>java.lang.List#E</td><td>String</td></tr>
+     * <tr><td>java.lang.Collection#E</td><td>String</td></tr>
+     * <tr><td>java.lang.Iterable#T</td><td>String</td></tr>
+     * </table>
+     * <p>
+     * If we want to determine the return type of {@link java.util.List#stream()} in our example we would look up
+     * <tt>java.lang.Collection#E</tt> which results in <tt>Stream&lt;String&gt;</tt>.
      *
      * @param rootType the type to derive parameterization from
      */
     public TypeTools(Type rootType) {
-        if (!(rootType instanceof ParameterizedType)) {
-            return;
+        propagateParameters(rootType);
+        reduceTable();
+    }
+
+    /**
+     * Propagates all type parameters for the whole inheritance tree for the given type.
+     *
+     * @param type the type to extract the type parameters from
+     */
+    private void propagateParameters(Type type) {
+        if (type instanceof ParameterizedType) {
+            propagateParameterizedTypeParameters((ParameterizedType) type);
         }
 
-        ParameterizedType parameterizedType = (ParameterizedType) rootType;
+        if (type instanceof Class<?>) {
+            propagateClassParameters((Class<?>) type);
+        }
+    }
+
+    /**
+     * Propagates all type parameters for the whole inheritance tree for the given class.
+     * <p>
+     * This will propagate the superclass and the interfaces of this class.
+     *
+     * @param clazz the class to extract the type parameters from
+     */
+    private void propagateClassParameters(Class<?> clazz) {
+        propagateParameters(clazz.getGenericSuperclass());
+
+        Type[] interfaces = clazz.getGenericInterfaces();
+        for (int i = 0; i < interfaces.length; i++) {
+            propagateParameters(interfaces[i]);
+        }
+    }
+
+    /**
+     * Propagates all type parameters for the whole inheritance tree for the given parameterized type.
+     * <p>
+     * This will add the type parameters for the given parameterized type to our table. Afterwards the class that is
+     * represented by this type will be propagated.
+     *
+     * @param parameterizedType the parameterized type to extract the type parameters from
+     */
+    private void propagateParameterizedTypeParameters(ParameterizedType parameterizedType) {
         if (!(parameterizedType.getRawType() instanceof Class<?>)) {
             return;
         }
 
-        TypeVariable<?>[] variables = ((Class<?>) parameterizedType.getRawType()).getTypeParameters();
+        Class<?> rawType = (Class<?>) parameterizedType.getRawType();
+        TypeVariable<?>[] variables = rawType.getTypeParameters();
         Type[] actualParameters = parameterizedType.getActualTypeArguments();
+
         for (int i = 0; i < variables.length; i++) {
-            addTypeVariableInfo(variables[i].getName(), actualParameters[i]);
+            addTypeVariableInfo(rawType, variables[i].getName(), actualParameters[i]);
         }
 
-        reduceTable();
+        propagateClassParameters(rawType);
     }
 
     /**
@@ -60,17 +128,20 @@ public class TypeTools {
      * @return the type tool itself for fluent method calls
      */
     public TypeTools withMethod(Method method, Node[] parameters) {
+        Class<?> scope = method.getDeclaringClass();
+
         for (TypeVariable<?> methodVariable : method.getTypeParameters()) {
-            addTypeVariableInfo(methodVariable.getName(),
+            addTypeVariableInfo(scope,
+                                methodVariable.getName(),
                                 methodVariable.getBounds().length > 0 ? methodVariable.getBounds()[0] : Object.class);
         }
 
         for (int i = 0; i < method.getParameterCount() && i < parameters.length; i++) {
             Type parameterType = method.getGenericParameterTypes()[i];
             Node parameter = parameters[i];
-            propagateConstantClassTypeInfos(parameterType, parameter);
-            propagateConstantValueTypeInfos(parameterType, parameter);
-            propagateConstantValueArrayTypeInfos(parameterType, parameter);
+            propagateConstantClassTypeInfos(scope, parameterType, parameter);
+            propagateConstantValueTypeInfos(scope, parameterType, parameter);
+            propagateConstantValueArrayTypeInfos(scope, parameterType, parameter);
         }
 
         reduceTable();
@@ -78,7 +149,7 @@ public class TypeTools {
         return this;
     }
 
-    private void propagateConstantClassTypeInfos(Type parameterType, Node parameter) {
+    private void propagateConstantClassTypeInfos(Class<?> scope, Type parameterType, Node parameter) {
         // Ensure that the parameter is of type Class<X> - abort otherwise
         if (!(parameterType instanceof ParameterizedType)) {
             return;
@@ -102,11 +173,11 @@ public class TypeTools {
 
         // For a constant class, we know the resulting type...
         if (parameter.isConstant() && Class.class.isAssignableFrom(parameter.getType())) {
-            addTypeVariableInfo(typeVariableName, (Class<?>) parameter.getConstantValue());
+            addTypeVariableInfo(scope, typeVariableName, (Class<?>) parameter.getConstantValue());
         }
     }
 
-    private void propagateConstantValueTypeInfos(Type parameterType, Node parameter) {
+    private void propagateConstantValueTypeInfos(Class<?> scope, Type parameterType, Node parameter) {
         // Ensure that the parameter is of type "T" (or the like) - abort otherwise
         if (!(parameterType instanceof TypeVariable)) {
             return;
@@ -116,10 +187,10 @@ public class TypeTools {
         Type genericType = parameter.getGenericType();
         Type type = genericType == null ? parameter.getType() : genericType;
 
-        addTypeVariableInfo(typeVariableName, type);
+        addTypeVariableInfo(scope, typeVariableName, type);
     }
 
-    private void propagateConstantValueArrayTypeInfos(Type parameterType, Node parameter) {
+    private void propagateConstantValueArrayTypeInfos(Class<?> scope, Type parameterType, Node parameter) {
         // Ensure that the parameter is of type "T" (or the like) - abort otherwise
         if (!(parameterType instanceof GenericArrayType)
             || !(((GenericArrayType) parameterType).getGenericComponentType() instanceof TypeVariable)) {
@@ -129,18 +200,18 @@ public class TypeTools {
         String typeVariableName =
                 ((TypeVariable<?>) ((GenericArrayType) parameterType).getGenericComponentType()).getName();
         if (parameter.getType().isArray()) {
-            addTypeVariableInfo(typeVariableName, parameter.getType().getComponentType());
+            addTypeVariableInfo(scope, typeVariableName, parameter.getType().getComponentType());
         } else {
             // seems to be a var args parameter..
             Type genericType = parameter.getGenericType();
-            addTypeVariableInfo(typeVariableName, genericType == null ? parameter.getType() : genericType);
+            addTypeVariableInfo(scope, typeVariableName, genericType == null ? parameter.getType() : genericType);
         }
     }
 
-    private void addTypeVariableInfo(String typeVariableName, Type type) {
+    private void addTypeVariableInfo(Class<?> scope, String typeVariableName, Type type) {
         // Ignore void as this info is useless for us and we might even override good info
         if (type != void.class) {
-            typeTable.put(typeVariableName, type);
+            typeTable.put(getTypeVariableKey(scope, typeVariableName), type);
         }
     }
 
@@ -156,7 +227,9 @@ public class TypeTools {
      */
     public Type simplify(Type type) {
         if (type instanceof TypeVariable<?>) {
-            return typeTable.getOrDefault(((TypeVariable<?>) type).getName(), type);
+            TypeVariable<?> typeVariable = (TypeVariable<?>) type;
+            return typeTable.getOrDefault(getTypeVariableKey(getScopeOfType(typeVariable), typeVariable.getName()),
+                                          type);
         }
         if (type instanceof WildcardType) {
             if (((WildcardType) type).getLowerBounds().length > 0) {
@@ -194,6 +267,32 @@ public class TypeTools {
                 return null;
             }
         };
+    }
+
+    @Nonnull
+    private String getTypeVariableKey(Class<?> scope, String typeVariableName) {
+        return scope.getName() + "#" + typeVariableName;
+    }
+
+    /**
+     * Determines the scope of the given type variable.
+     *
+     * @param typeVariable the type variable to determine the scope for
+     * @return the class that represents the scope
+     */
+    private Class<?> getScopeOfType(TypeVariable<?> typeVariable) {
+        GenericDeclaration genericDeclaration = typeVariable.getGenericDeclaration();
+
+        if (genericDeclaration instanceof Executable) {
+            return ((Executable) genericDeclaration).getDeclaringClass();
+        }
+
+        if (genericDeclaration instanceof Class) {
+            return (Class<?>) genericDeclaration;
+        }
+
+        // Shouldn't happen as only Executable and Class are implementing GenericDeclaration in java.lang.reflect
+        throw new UnsupportedOperationException("The TypeVariable " + typeVariable + " is not supported!");
     }
 
     /**
