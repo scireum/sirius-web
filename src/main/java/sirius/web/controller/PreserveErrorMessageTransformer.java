a29595c861e7d7f90e8d800192f1f194b9d4eb7c
Andreas Haufler <aha@scireum.de>
2021-06-15 16:12:01 +0200

    Outputs Tagliatelle compilation errors properly.
    We use a monospaced font and preserve whitespaces to properly view
    the hints and code locations as generated by Noodle/Tagliatelle.

---
+++ src/main/java/sirius/web/controller/PreserveErrorMessageTransformer.java
@@ -0,0 +1,38 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.web.controller;
+
+import sirius.kernel.di.std.Register;
+import sirius.kernel.health.ExceptionHint;
+import sirius.kernel.health.HandledException;
+
+/**
+ * Outputs the whole error message while preserving whitespaces.
+ * <p>
+ * This is mainly used to properly report <b>Tagliatelle</b> errors.
+ */
+@Register
+public class PreserveErrorMessageTransformer implements ErrorMessageTransformer {
+
+    public static final ExceptionHint PRESERVE = new ExceptionHint("preserve");
+
+    @Override
+    public String transform(HandledException exception, String message) {
+        if (exception.getHint(PRESERVE).asBoolean()) {
+            return "<div class=\"pre-message\">" + message + "</div>";
+        } else {
+            return message;
+        }
+    }
+
+    @Override
+    public int getPriority() {
+        return 100;
+    }
+}
