4c30f626724137d54287b390a3a7f7af10855d3e
Marc Egenrieder <meg@scireum.de>
2021-08-24 08:57:44 +0200

    Ports delete link template to tycho
    Fixes: OX-7191

--- src/main/resources/default/assets/tycho/scripts/form.js
+++ src/main/resources/default/assets/tycho/scripts/form.js
@@ -18,4 +18,51 @@ sirius.ready(function () {
             }
         });
     });
+
+    document.querySelectorAll('.danger-link').forEach(function (_node) {
+        _node.addEventListener('click', function () {
+            if (_node.matches('.guarded-link')) {
+                try {
+                    let modalElement = document.getElementById('danger-link-confirm');
+                    let submitBtn = modalElement.querySelector("button[type='submit']");
+
+                    submitBtn.addEventListener('click', function() {
+                        document.getElementById(_node.getAttribute('data-deleteid')).submit();
+                    });
+
+                    $('#danger-link-confirm').modal({
+                        keyboard: true
+                    }).on('shown', function () {
+                        modalElement.querySelector('.btn-close').focus();
+                    }).attr('tabindex', -1);
+                } catch (e) {
+                    console.log(e);
+                }
+
+                return false;
+            } else {
+                try {
+                    let modalElement = document.getElementById('danger-link-confirm');
+
+                    let okayLink = _node.getAttribute('href');
+                    let submitBtn = modalElement.querySelector("button[type='submit']");
+
+                    submitBtn.addEventListener('click', function() {
+                        window.location.href = okayLink;
+                    });
+
+                    $('#danger-link-confirm').modal({
+                        keyboard: true
+                    }).on('shown', function () {
+                        modalElement.querySelector('.btn-close').focus();
+                    }).setAttribute('tabindex', -1);
+                } catch (e) {
+                    console.log(e);
+                }
+
+                return false;
+            }
+        });
+    });
 });
+
