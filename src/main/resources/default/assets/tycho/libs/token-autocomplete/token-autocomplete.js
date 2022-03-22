da1d4ff356b8a1c46ea557b2cf96f414ae5495d7
Sascha Bieberstein <sbi@scireum.de>
2022-03-22 15:39:16 +0100

    Upgrades token autocomplete JS lib to latest version
    This includes the changes of https://github.com/scireum/token-autocomplete/pull/57

--- src/main/resources/default/assets/tycho/libs/token-autocomplete/token-autocomplete.js
+++ src/main/resources/default/assets/tycho/libs/token-autocomplete/token-autocomplete.js
@@ -738,7 +738,7 @@ var TokenAutocomplete = /** @class */ (function () {
                         }
                         return;
                     }
-                    if (event.key == me.parent.KEY_LEFT || event.key == me.parent.KEY_RIGHT || event.key == me.parent.KEY_ENTER) {
+                    if (event.key == me.parent.KEY_LEFT || event.key == me.parent.KEY_RIGHT || event.key == me.parent.KEY_ENTER || event.key == me.parent.KEY_TAB) {
                         // We don't want to re-trigger the autocompletion when the user navigates the cursor inside the input.
                         return;
                     }
