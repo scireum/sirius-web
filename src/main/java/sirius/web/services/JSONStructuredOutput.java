821c925d178360bc468842b97c8d6aea41fde0fe
Idevaldo De Lira <ili@scireum.de>
2022-05-24 16:41:18 +0200

    Extracts a method
    Fixes: OX-8608

--- src/main/java/sirius/web/services/JSONStructuredOutput.java
+++ src/main/java/sirius/web/services/JSONStructuredOutput.java
@@ -234,10 +234,7 @@ public class JSONStructuredOutput extends AbstractStructuredOutput {
     private void writePlainProperty(String name, Object data) {
         try {
             addRequiredComma();
-            if (getCurrentType() == ElementType.OBJECT) {
-                writeString(name);
-                writer.write(":");
-            }
+            addObjectName(name);
             if (data == null) {
                 writer.write("null");
             } else if (data instanceof Boolean || data instanceof Number) {
@@ -262,13 +259,17 @@ public class JSONStructuredOutput extends AbstractStructuredOutput {
     protected void writeAmountProperty(String name, String formattedAmount) {
         try {
             addRequiredComma();
-            if (getCurrentType() == ElementType.OBJECT) {
-                writeString(name);
-                writer.write(":");
-            }
+            addObjectName(name);
             writer.write(Strings.isFilled(formattedAmount) ? formattedAmount : "null");
         } catch (IOException e) {
             throw Exceptions.handle(e);
+        }
+    }
+
+    private void addObjectName(String name) throws IOException {
+        if (getCurrentType() == ElementType.OBJECT) {
+            writeString(name);
+            writer.write(":");
         }
     }
 
