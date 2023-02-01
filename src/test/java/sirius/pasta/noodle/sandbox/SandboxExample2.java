c1a9f503a71d6e93a330121f6e9447774aa7081a
Jan Müller <jmu@scireum.de>
2023-02-01 15:13:18 +0100

    Adds some test cases for Sandboxing

---
+++ src/test/java/sirius/pasta/noodle/sandbox/SandboxExample2.java
@@ -0,0 +1,28 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.pasta.noodle.sandbox;
+
+public class SandboxExample2 extends SandboxExample {
+
+    @Override
+    public String grantedMethod() {
+        return "granted";
+    }
+
+    @Override
+    public String noAnnotation() {
+        return "rejected";
+    }
+
+    @Override
+    @NoodleSandbox(NoodleSandbox.Accessibility.GRANTED)
+    public String noAnnotation2() {
+        return "granted";
+    }
+}
