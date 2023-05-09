b1f9972b4113ee3c5ad33b8519f08c6ff9b2b28a
Christian Schierle <csc@scireum.de>
2023-05-09 12:53:48 +0200

    Properly configures test suite. Gradle tasks `test` and `testWithoutNightly` run Groovy Specs via "TestSuite" configurations under JUnit 4, while Java and Kotlin tests (named `...Test.(kt|java)`) are run under JUnit 5 with "SiriusExtension"s where applicable.
    Fixes: SIRI-647

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -7,29 +7,33 @@
  */
 
 import com.googlecode.junittoolbox.SuiteClasses;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.runner.RunWith;
 import sirius.kernel.ScenarioSuite;
+import sirius.kernel.TestHelper;
 
 // JUnit 4 annotations below
 @RunWith(ScenarioSuite.class)
-@SuiteClasses({"**/*Test.class", "**/*Spec.class"})
-// JUnit 5 annotations below
-@Suite
-@IncludeClassNamePatterns({"^.*Test$", "^.*Spec$"})
-@SelectPackages("sirius")
+@SuiteClasses("**/*Spec.class")
 public class TestSuite {
 
     @BeforeClass
-    @BeforeAll
     public static void setUp() {
         // Allow us to set the Origin: header...
         // This is also set in the CORSSpec, but if the whole TestSuite is executed,
         // this might be too late as a previously opened url connection might be re-used...
         System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+        TestHelper.setUp(TestSuite.class);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        TestHelper.tearDown(TestSuite.class);
     }
 }
