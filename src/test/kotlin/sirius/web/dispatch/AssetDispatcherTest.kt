62cb44dd1208f465dff10bdc970bc0216caef8ff
MOOOOOSER <moser.sandro222@googlemail.com>
2023-12-06 14:05:18 +0100

    adapted Kotlin Syntax

--- src/test/kotlin/sirius/web/dispatch/AssetDispatcherTest.kt
+++ src/test/kotlin/sirius/web/dispatch/AssetDispatcherTest.kt
@@ -9,39 +9,54 @@
 package sirius.web.dispatch
 
 import io.netty.handler.codec.http.HttpHeaderNames
-import sirius.kernel.BaseSpecification
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import sirius.kernel.SiriusExtension
 import sirius.web.sass.Output
+import java.io.StringWriter
+import java.net.URL
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
-class AssetsDispatcherSpec extends BaseSpecification {
 
-    def "proper caching set for all kind of assets and cache-control URIs"() {
-        expect:
-        URLConnection c = new URL("http://localhost:9999" + uri).openConnection()
-        c.getHeaderField(HttpHeaderNames.CACHE_CONTROL.toString()) == header
-        where:
-        uri                               | header
-        '/assets/test/test.css'           | 'public, max-age=21600'
-        '/assets/test/test.txt'           | 'public, max-age=21600'
-        '/assets/test/test.js'            | 'public, max-age=21600'
-        '/assets/no-cache/test/test.css'  | 'no-cache, max-age=0'
-        '/assets/no-cache/test/test.txt'  | 'no-cache, max-age=0'
-        '/assets/no-cache/test/test.js'   | 'no-cache, max-age=0'
-        '/assets/dynamic/X/test/test.css' | 'public, max-age=615168000'
-        '/assets/dynamic/X/test/test.txt' | 'public, max-age=615168000'
-        '/assets/dynamic/X/test/test.js'  | 'public, max-age=615168000'
+/**
+ * Tests the [AssetsDispatcher] class.
+ */
+@ExtendWith(SiriusExtension::class)
+class AssetsDispatcherTest {
+    @CsvSource(
+        delimiter = '|', useHeadersInDisplayName = true, textBlock = """
+         uri                            | header
+        /assets/test/test.css           | public, max-age=21600
+        /assets/test/test.txt           | public, max-age=21600
+        /assets/test/test.js            | public, max-age=21600
+        /assets/no-cache/test/test.css  | no-cache, max-age=0
+        /assets/no-cache/test/test.txt  | no-cache, max-age=0
+        /assets/no-cache/test/test.js   | no-cache, max-age=0
+        /assets/dynamic/X/test/test.css | public, max-age=615168000
+        /assets/dynamic/X/test/test.txt | public, max-age=615168000
+        /assets/dynamic/X/test/test.js  | 'public, max-age=615168000"""
+    )
+    @ParameterizedTest
+    fun `proper caching set for all kind of assets and cache-control URIs`(uri: String, header: String) {
+        val connection = URL("http://localhost:9999" + uri).openConnection()
+        assertEquals(header, connection.getHeaderField(HttpHeaderNames.CACHE_CONTROL.toString()))
+
     }
 
-    def "Custom base64ResourceFunction works"() {
-        when:
-        SiriusSassGenerator gen = new SiriusSassGenerator()
-        gen.importStylesheet("/assets/test_base64.scss")
-        gen.compile()
+    @Test
+    fun `Custom base64ResourceFunction works`() {
+        val generator = SiriusSassGenerator()
+        generator.importStylesheet("/assets/test_base64.scss")
+        generator.compile()
 
-        StringWriter writer = new StringWriter()
-        gen.generate(new Output(writer, true))
-        then:
-        writer
-            .toString() == "test { background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QsODw4S4KU/XgAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAdElEQVRYw+3Q3Q1AMAAA4ar+qHhghq7SBQ3CPsQO5a1JS0wh+nA3wZdr5hBEfUlRZbBgwYIFCxYsWLBgwYIFCxYsWLBgwYIFCxYsWLBgfZSqDRRtWv1eC2vx2zFGV7S5pX2U+n3M2SWX1ZCNv6a+aHNL/bQvbxUXkThEKBQAAAAASUVORK5CYII=); }\n "
+        val writer = StringWriter()
+        generator.generate(Output(writer, true))
+        assertEquals(
+            "test { background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAIAAACRXR/mAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QsODw4S4KU/XgAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAdElEQVRYw+3Q3Q1AMAAA4ar+qHhghq7SBQ3CPsQO5a1JS0wh+nA3wZdr5hBEfUlRZbBgwYIFCxYsWLBgwYIFCxYsWLBgwYIFCxYsWLBgfZSqDRRtWv1eC2vx2zFGV7S5pX2U+n3M2SWX1ZCNv6a+aHNL/bQvbxUXkThEKBQAAAAASUVORK5CYII=); }\n ",
+            writer.toString()
+        )
     }
 
 }
