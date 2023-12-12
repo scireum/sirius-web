cba33cc8cb3a11b955189664e136ef19d7f1f34e
MOOOOOSER <moser.sandro222@googlemail.com>
2023-12-12 15:01:09 +0100

    moved file to new place

---
+++ src/test/kotlin/sirius/web/http/UploadTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.web.http
+
+import org.junit.jupiter.api.Tag
+import sirius.kernel.BaseSpecification
+import sirius.kernel.Tags
+import sirius.kernel.commons.Json
+import sirius.kernel.commons.Streams
+
+import java.nio.charset.StandardCharsets
+
+/**
+ * Simulates "real" uploads through netty and sirius.
+ */
+@Tag(Tags.NIGHTLY)
+class UploadSpec extends BaseSpecification {
+
+    String lineFeed = "\r\n"
+
+    def upload(String uri, File file) {
+        HttpURLConnection connection = new URL("http://localhost:9999" + uri).openConnection()
+
+        InputStream inputStream = new FileInputStream(file)
+        connection.setDoOutput(true)
+        connection.setRequestMethod("POST")
+        OutputStream outputStream = connection.getOutputStream()
+        Streams.transfer(inputStream, outputStream)
+        inputStream.close()
+        outputStream.flush()
+
+        return new String(Streams.toByteArray(connection.getInputStream()), StandardCharsets.UTF_8)
+    }
+
+    def "Uploads a file to /upload-test"() {
+        given:
+        File file = new File("src/test/resources/test.png")
+        when:
+        def response = upload("/upload-test", file)
+        and:
+        def json = Json.parseObject(response)
+        then:
+        json.get("success").asBoolean() == true
+        and:
+        json.get("size").asLong() == file.length()
+    }
+
+    def "Uploads a gzip-file to /upload-gzip"() {
+        given:
+        File file = new File("src/test/resources/test.csv.gz")
+        when:
+        def response = upload("/upload-gzip", file)
+        and:
+        def json = Json.parseObject(response)
+        then:
+        json.get("success").asBoolean() == true
+        and:
+        json.get("lines").asLong() == 3
+    }
+}
