1564e8af1297c107ca110272ca48d86d50cbdd39
MOOOOOSER <moser.sandro222@googlemail.com>
2023-12-14 11:17:12 +0100

    adapted kotlin syntax

--- src/test/kotlin/sirius/web/http/CorsTest.kt
+++ src/test/kotlin/sirius/web/http/CorsTest.kt
@@ -10,36 +10,43 @@ package sirius.web.http
 
 import io.netty.handler.codec.http.HttpHeaderNames
 import io.netty.handler.codec.http.HttpMethod
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-class CORSSpec extends BaseSpecification {
-
-    def "expect 'Access-Control-Allow-Origin' for requests with 'origin'"() {
-        when:
-        HttpURLConnection c = new URL("http://localhost:9999/system/ok").openConnection()
-        // Setting the "Origin: header" must be allowed by -Dsun.net.http.allowRestrictedHeaders=true
-        c.addRequestProperty("Origin", "TEST")
-
-        then:
-        c.getInputStream().close()
-        c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString()) == "TEST"
-    }
-
-    def "expect a CORS preflight request to be answered correctly"() {
-        when:
+@ExtendWith(SiriusExtension::class)
+class CORSTest {
+    @Test
+    fun `expect 'Access-Control-Allow-Origin' for requests with 'origin'`() {
         // Allow us to set the Origin: header...
         System.setProperty("sun.net.http.allowRestrictedHeaders", "true")
-        HttpURLConnection c = new URL("http://localhost:9999/system/ok").openConnection()
+        val c = URL("http://localhost:9999/system/ok").openConnection() as HttpURLConnection
+        // Setting the "Origin: header" must be allowed by -Dsun.net.http.allowRestrictedHeaders=true
+
+        c.addRequestProperty("Origin", "TEST")
+        c.getInputStream().close()
+        assertEquals("TEST", c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString()))
+    }
+
+    @Test
+    fun `expect a CORS preflight request to be answered correctly`() {
+        // Allow us to set the Origin: header...
+        System.setProperty("sun.net.http.allowRestrictedHeaders", "true")
+        val c = URL("http://localhost:9999/system/ok").openConnection() as HttpURLConnection
         c.setRequestMethod(HttpMethod.OPTIONS.name())
         c.addRequestProperty("Origin", "TEST")
         c.addRequestProperty("Access-Control-Request-Method", "GET")
         c.addRequestProperty("Access-Control-Request-Headers", "X-Test")
 
-        then:
         c.getInputStream().close()
-        c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString()) == "TEST"
-        c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_METHODS.toString()).indexOf("GET") >= 0
-        c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_HEADERS.toString()).indexOf("X-Test") >= 0
+        assertEquals("TEST", c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString()))
+        assertTrue { c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_METHODS.toString()).indexOf("GET") >= 0 }
+        assertTrue { c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_HEADERS.toString()).indexOf("X-Test") >= 0 }
     }
 
 }
