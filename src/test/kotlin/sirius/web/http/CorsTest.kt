76a49d17fc40ca417947a9913f8c9b6817e9123a
MOOOOOSER <moser.sandro222@googlemail.com>
2023-12-14 12:26:24 +0100

    changed URL to URI, renamed c to connection

--- src/test/kotlin/sirius/web/http/CorsTest.kt
+++ src/test/kotlin/sirius/web/http/CorsTest.kt
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import sirius.kernel.BaseSpecification
 import sirius.kernel.SiriusExtension
 import java.net.HttpURLConnection
+import java.net.URI
 import java.net.URL
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -25,27 +26,27 @@ class CORSTest {
     fun `expect 'Access-Control-Allow-Origin' for requests with 'origin'`() {
         // Allow us to set the Origin: header...
         System.setProperty("sun.net.http.allowRestrictedHeaders", "true")
-        val c = URL("http://localhost:9999/system/ok").openConnection() as HttpURLConnection
+        val connection = URI("http://localhost:9999/system/ok").toURL().openConnection() as HttpURLConnection
         // Setting the "Origin: header" must be allowed by -Dsun.net.http.allowRestrictedHeaders=true
-        c.addRequestProperty("Origin", "TEST")
-        c.getInputStream().close()
-        assertEquals("TEST", c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString()))
+        connection.addRequestProperty("Origin", "TEST")
+        connection.getInputStream().close()
+        assertEquals("TEST", connection.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString()))
     }
 
     @Test
     fun `expect a CORS preflight request to be answered correctly`() {
         // Allow us to set the Origin: header...
         System.setProperty("sun.net.http.allowRestrictedHeaders", "true")
-        val c = URL("http://localhost:9999/system/ok").openConnection() as HttpURLConnection
-        c.setRequestMethod(HttpMethod.OPTIONS.name())
-        c.addRequestProperty("Origin", "TEST")
-        c.addRequestProperty("Access-Control-Request-Method", "GET")
-        c.addRequestProperty("Access-Control-Request-Headers", "X-Test")
+        val connection = URI("http://localhost:9999/system/ok").toURL().openConnection() as HttpURLConnection
+        connection.setRequestMethod(HttpMethod.OPTIONS.name())
+        connection.addRequestProperty("Origin", "TEST")
+        connection.addRequestProperty("Access-Control-Request-Method", "GET")
+        connection.addRequestProperty("Access-Control-Request-Headers", "X-Test")
 
-        c.getInputStream().close()
-        assertEquals("TEST", c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString()))
-        assertTrue { c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_METHODS.toString()).indexOf("GET") >= 0 }
-        assertTrue { c.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_HEADERS.toString()).indexOf("X-Test") >= 0 }
+        connection.getInputStream().close()
+        assertEquals("TEST", connection.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString()))
+        assertTrue { connection.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_METHODS.toString()).indexOf("GET") >= 0 }
+        assertTrue { connection.getHeaderField(HttpHeaderNames.ACCESS_CONTROL_ALLOW_HEADERS.toString()).indexOf("X-Test") >= 0 }
     }
 
 }
