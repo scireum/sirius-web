78380ee52d15e8f7d76a5e0542f2800e40a68fe6
MOOOOOSER <moser.sandro222@googlemail.com>
2023-12-15 11:17:33 +0100

    adapted Kotlin Syntax

--- src/test/kotlin/sirius/web/health/ConsoleControllerTest.kt
+++ src/test/kotlin/sirius/web/health/ConsoleControllerTest.kt
@@ -6,12 +6,14 @@
  * http://www.scireum.de - info@scireum.de
  */
 
-
 package sirius.web.health
 
 import com.fasterxml.jackson.core.JsonPointer
 import io.netty.handler.codec.http.HttpResponseStatus
-import sirius.kernel.BaseSpecification
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.kernel.SiriusExtension
 import sirius.kernel.commons.Context
 import sirius.kernel.commons.Json
 import sirius.web.health.console.ConsoleController
@@ -19,42 +21,38 @@ import sirius.web.http.TestRequest
 import sirius.web.http.TestResponse
 import sirius.web.security.UserContext
 import sirius.web.security.UserInfo
-
-class ConsoleControllerSpec extends BaseSpecification {
-
-    def "/system/console renders its template"() {
-        given:
+import kotlin.test.assertEquals
+import java.util.Collections
+@ExtendWith(SiriusExtension::class)
+class ConsoleControllerTest {
+    @Test
+    fun ` system console renders its template`() {
         UserContext.get().setCurrentUser(UserInfo.Builder.createUser("test")
             .withPermissions(Collections.
             singleton(ConsoleController.PERMISSION_SYSTEM_CONSOLE))
             .build())
-        when:
-        def result = TestRequest.GET("/system/console").execute()
-        then:
-        result.getStatus() == HttpResponseStatus.OK
-        result.getType() == TestResponse.ResponseType.TEMPLATE
-        result.getTemplateName() == "/templates/system/console.html.pasta"
+        val result = TestRequest.GET("/system/console").execute()
+        assertEquals(HttpResponseStatus.OK,result.getStatus())
+        assertEquals(TestResponse.ResponseType.TEMPLATE, result.getType())
+        assertEquals("/templates/system/console.html.pasta", result.getTemplateName())
     }
 
-    def "/system/console/api returns JSON for help"() {
-        when:
+    @Test
+    fun `system console api returns JSON for help`() {
         UserContext.get().setCurrentUser(UserInfo.
         Builder.
         createUser("test").
         withPermissions(Collections.singleton(ConsoleController.PERMISSION_SYSTEM_CONSOLE)).
         build())
-        def result = TestRequest.
+        val result = TestRequest.
         POST("/system/console/api").
         withParameters(Context.create().set("command", "help")).
         execute()
-        and:
-        def json = result.getContentAsJson()
-        then:
-        result.getStatus() == HttpResponseStatus.OK
-        Json.tryGetAt(json, JsonPointer.compile("/error/code"))
-            .map { Json.convertToValue(it) }.map { it.isEmptyString() }.orElse(true)
-        Json.tryGetAt(json, JsonPointer.compile("/result"))
-            .map { Json.convertToValue(it) }.map { it.isFilled() }.orElse(false)
+        val json = result.getContentAsJson()
+        assertEquals(HttpResponseStatus.OK,result.getStatus())
+        assertTrue{Json.tryGetAt(json, JsonPointer.compile("/error/code"))
+            .map { Json.convertToValue(it) }.map { it.isEmptyString()}.orElse(true) }
+        assertTrue{ Json.tryGetAt(json, JsonPointer.compile("/result"))
+            .map { Json.convertToValue(it) }.map { it.isFilled() }.orElse(false)}
     }
-
 }
