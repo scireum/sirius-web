27eeabc95d1d926de2698c8575c029be1a4a4b1d
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-08 16:38:33 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/web/mails/MailsTest.kt
+++ src/test/kotlin/sirius/web/mails/MailsTest.kt
@@ -8,49 +8,61 @@
 
 package sirius.web.mails
 
-import sirius.kernel.BaseSpecification
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.kernel.SiriusExtension
 import sirius.kernel.commons.Context
 import sirius.kernel.di.std.Part
 import sirius.kernel.health.HandledException
+import kotlin.test.assertEquals
 
-class MailsSpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class MailsTest {
 
-    @Part
-    static Mails mails
+    companion object {
+        @Part
+        @JvmStatic
+        private lateinit var mails: MailsMock
+    }
 
-    def "Mails sends a simple mail"() {
-        when:
-        ((MailsMock) mails).getSentMails().clear()
+    @Test
+    fun `Mails sends a simple mail`() {
+
+        mails.sentMails.clear()
         mails.createEmail().to("test@scireum.de", "Test").subject("Test eMail").textContent("This is a Test.").send()
-        then:
-        MailsMock.MailSenderMock mail = ((MailsMock) mails).getLastMail()
-        mail.getReceiverEmail() == "test@scireum.de"
-        mail.getReceiverName() == "Test"
-        mail.getSubject() == "Test eMail"
-        mail.getText() == "This is a Test."
-        mail.getHtml() == null
+        println(mails.lastMail.receiverName)
+
+        val mail = mails.lastMail
+        assertEquals("test@scireum.de", mail.getReceiverEmail())
+        assertEquals("Test", mail.getReceiverName())
+        assertEquals("Test eMail", mail.getSubject())
+        assertEquals("This is a Test.", mail.getText())
+        assertEquals(null, mail.getHtml())
     }
 
-    def "Mails rejects an invalid receiver"() {
-        when:
-        ((MailsMock) mails).getSentMails().clear()
-        mails.createEmail().to("test@", "Invalid").subject("Test eMail").textContent("This is a Test.").send()
-        then:
-        thrown(HandledException)
+    @Test
+    fun `Mails rejects an invalid receiver`() {
+        mails.sentMails.clear()
+
+        assertThrows<HandledException> {
+            mails.createEmail().to("test@", "Invalid").subject("Test eMail").textContent("This is a Test.").send()
+        }
     }
 
-    def "Mails translates subject to correct language"() {
-        when:
-        ((MailsMock) mails).getSentMails().clear()
+    @Test
+    fun `Mails translates subject to correct language`() {
+
+        mails.sentMails.clear()
         mails.createEmail()
             .to("test@scireum.de", "Test")
             .nlsSubject("mail.subject", Context.create().set("nr", "1"))
             .setLanguage("fr")
             .textContent("This is a Test.")
             .send()
-        then:
-        MailsMock.MailSenderMock mail = ((MailsMock) mails).getLastMail()
-        mail.getSubject() == "Ceci est le test 1."
+
+        val mail = mails.lastMail
+        assertEquals("Ceci est le test 1.", mail.getSubject())
     }
 
 }
