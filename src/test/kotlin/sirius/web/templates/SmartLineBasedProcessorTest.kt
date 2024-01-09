5f8c39cb37382ea6bfbce2f35fa997b83cb8f1d3
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-09 11:29:38 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/web/templates/SmartLineBasedProcessorTest.kt
+++ src/test/kotlin/sirius/web/templates/SmartLineBasedProcessorTest.kt
@@ -9,49 +9,57 @@
 package sirius.web.templates
 
 import com.google.common.collect.Lists
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.web.data.LineBasedProcessor
 import sirius.web.data.NamedRowProcessor
 import sirius.web.data.SmartLineBasedProcessor
 import sirius.web.data.SmartRow
+import java.lang.Exception
 
 import java.util.function.Predicate
+import kotlin.reflect.KClass
+import kotlin.test.assertEquals
 
-class SmartLineBasedProcessorSpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class SmartLineBasedProcessorTest {
 
-    def "reading CSVs works with different column orders and aliases"() {
-        given:
-        List<SmartRow> contents1 = Lists.newArrayList()
-        List<SmartRow> contents2 = Lists.newArrayList()
-        SmartLineBasedProcessor proc1 = new SmartLineBasedProcessor()
+    @Test
+    fun `reading CSVs works with different column orders and aliases`() {
+        
+        val contents1 = Lists.newArrayList<SmartRow>()
+        val contents2 = Lists.newArrayList<SmartRow>()
+        val proc1 = SmartLineBasedProcessor()
             .withColumn("item", "artikel")
             .withColumn("quantity")
-            .withProcessor({ l, v -> contents1.add(v) } as NamedRowProcessor)
-        SmartLineBasedProcessor proc2 = new SmartLineBasedProcessor()
+            .withProcessor { line: Int, row: SmartRow -> contents1.add(row) }
+        val proc2 = SmartLineBasedProcessor()
             .withColumn("item", "artikel")
             .withColumn("quantity")
-            .withProcessor({ l, v -> contents2.add(v) } as NamedRowProcessor)
+            .withProcessor { line: Int, row: SmartRow -> contents2.add(row) }
 
-        LineBasedProcessor lineProc1 = LineBasedProcessor.create("smart-test1.csv", getClass().getResourceAsStream("/smart-test1.csv"))
-        LineBasedProcessor lineProc2 = LineBasedProcessor.create("smart-test2.csv", getClass().getResourceAsStream("/smart-test2.csv"))
-        when:
-        lineProc1.run(proc1, {e -> false} as Predicate)
-        lineProc2.run(proc2, {e -> false} as Predicate)
-        then:
-        contents1.size() == 2
-        contents2.size() == 2
-        and:
-        contents1.get(0).getFirst("item").asString() == 'A'
-        contents1.get(0).getFirst("quantity").asInt(-1) == 1
-        and:
-        contents2.get(0).getFirst("item").asString() == 'A'
-        contents2.get(0).getFirst("quantity").asInt(-1) == 1
-        and:
-        contents1.get(1).getFirst("item").asString() == 'B'
-        contents1.get(1).getFirst("quantity").asInt(-1) == 2
-        and:
-        contents2.get(1).getFirst("item").asString() == 'B'
-        contents2.get(1).getFirst("quantity").asInt(-1) == 2
+        val lineProc1 = LineBasedProcessor.create("smart-test1.csv", KClass::class.java.getResourceAsStream("/smart-test1.csv"))
+        val lineProc2 = LineBasedProcessor.create("smart-test2.csv", KClass::class.java.getResourceAsStream("/smart-test2.csv"))
+
+        lineProc1.run(proc1) { e: Exception? -> false }
+        lineProc2.run(proc2) { e: Exception? -> false }
+
+        assertEquals(2,contents1.size)
+        assertEquals(2,contents2.size)
+
+        assertEquals("A", contents1[0].getFirst("item").asString())
+        assertEquals(1, contents1[0].getFirst("quantity").asInt(-1))
+
+        assertEquals("A", contents2[0].getFirst("item").asString())
+        assertEquals(1, contents2[0].getFirst("quantity").asInt(-1))
+
+        assertEquals("B", contents1[1].getFirst("item").asString())
+        assertEquals(2, contents1[1].getFirst("quantity").asInt(-1))
+
+        assertEquals("B", contents2[1].getFirst("item").asString())
+        assertEquals(2, contents2[1].getFirst("quantity").asInt(-1))
     }
 
 
