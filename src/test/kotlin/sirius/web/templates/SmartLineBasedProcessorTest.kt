13075a4651e84cfa7ff04de3554a30376ff44a5c
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-09 11:30:36 +0100

    Formatted code
    Fixes: OX-10357

--- src/test/kotlin/sirius/web/templates/SmartLineBasedProcessorTest.kt
+++ src/test/kotlin/sirius/web/templates/SmartLineBasedProcessorTest.kt
@@ -11,15 +11,10 @@ package sirius.web.templates
 import com.google.common.collect.Lists
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import sirius.kernel.BaseSpecification
 import sirius.kernel.SiriusExtension
 import sirius.web.data.LineBasedProcessor
-import sirius.web.data.NamedRowProcessor
 import sirius.web.data.SmartLineBasedProcessor
 import sirius.web.data.SmartRow
-import java.lang.Exception
-
-import java.util.function.Predicate
 import kotlin.reflect.KClass
 import kotlin.test.assertEquals
 
@@ -28,7 +23,7 @@ class SmartLineBasedProcessorTest {
 
     @Test
     fun `reading CSVs works with different column orders and aliases`() {
-        
+
         val contents1 = Lists.newArrayList<SmartRow>()
         val contents2 = Lists.newArrayList<SmartRow>()
         val proc1 = SmartLineBasedProcessor()
@@ -40,14 +35,16 @@ class SmartLineBasedProcessorTest {
             .withColumn("quantity")
             .withProcessor { line: Int, row: SmartRow -> contents2.add(row) }
 
-        val lineProc1 = LineBasedProcessor.create("smart-test1.csv", KClass::class.java.getResourceAsStream("/smart-test1.csv"))
-        val lineProc2 = LineBasedProcessor.create("smart-test2.csv", KClass::class.java.getResourceAsStream("/smart-test2.csv"))
+        val lineProc1 =
+            LineBasedProcessor.create("smart-test1.csv", KClass::class.java.getResourceAsStream("/smart-test1.csv"))
+        val lineProc2 =
+            LineBasedProcessor.create("smart-test2.csv", KClass::class.java.getResourceAsStream("/smart-test2.csv"))
 
         lineProc1.run(proc1) { e: Exception? -> false }
         lineProc2.run(proc2) { e: Exception? -> false }
 
-        assertEquals(2,contents1.size)
-        assertEquals(2,contents2.size)
+        assertEquals(2, contents1.size)
+        assertEquals(2, contents2.size)
 
         assertEquals("A", contents1[0].getFirst("item").asString())
         assertEquals(1, contents1[0].getFirst("quantity").asInt(-1))
