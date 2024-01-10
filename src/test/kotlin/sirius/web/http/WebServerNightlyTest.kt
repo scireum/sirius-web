61ed57fdfdd1b9e33d0afd9df3a92cf117951707
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-10 10:21:32 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/web/http/WebServerNightlyTest.kt
+++ src/test/kotlin/sirius/web/http/WebServerNightlyTest.kt
@@ -9,15 +9,23 @@
 package sirius.web.http
 
 import io.netty.bootstrap.Bootstrap
-import io.netty.channel.*
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelInboundHandlerAdapter
+import io.netty.channel.ChannelInitializer
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioSocketChannel
 import io.netty.handler.codec.http.*
 import org.junit.jupiter.api.Tag
-import sirius.kernel.BaseSpecification
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.kernel.SiriusExtension
 import sirius.kernel.Tags
 import sirius.kernel.commons.Json
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URI
+import kotlin.test.assertEquals
 
 /**
  * Simulates a bunch of "real" (outside) requests through netty and sirius.
@@ -28,7 +36,8 @@ import sirius.kernel.commons.Json
  * This contains long running tests to be executed nightly.
  */
 @Tag(Tags.NIGHTLY)
-class WebServerNightlySpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class WebServerNightlyTest {
 
     /**
      * Invoke a web dispatcher which previously blocked the event loop and crashed netty.
@@ -38,85 +47,88 @@ class WebServerNightlySpec extends BaseSpecification {
      * Therefore the event loop can shovel away the data in the output buffer of the channel
      * and the future will eventually fullfilled.
      */
-    def "Invoke /large-blocking-calls with GET"() {
-        given:
-        HttpURLConnection u = new URL("http://localhost:9999/large-blocking-calls").openConnection()
-        when:
-        u.setRequestMethod("GET")
-        u.setDoOutput(false)
-        and:
-        def counter = countBytesInStream(u.getInputStream())
-        then:
-        180000000 == counter
+    @Test
+    fun `Invoke large-blocking-calls with GET`() {
+
+        val url = URI("http://localhost:9999/large-blocking-calls").toURL().openConnection() as HttpURLConnection
+
+        url.setRequestMethod("GET")
+        url.setDoOutput(false)
+
+        val counter = countBytesInStream(url.inputStream)
+
+        assertEquals(180000000, counter)
     }
 
-    private int countBytesInStream(InputStream input) {
-        def counter = 0
-        def count = 0
-        def buffer = new byte[8192]
-        while ((count = input.read(buffer)) > 0) {
+    private fun countBytesInStream(input: InputStream): Int {
+        var counter = 0
+        var count = 0
+        val buffer = ByteArray(8192)
+        while ((input.read(buffer).also { count = it }) > 0) {
             counter += count
         }
 
         return counter
     }
 
-    def "HTTP pipelining is supported correctly"() {
-        given:
-        List<HttpResponse> responses = new ArrayList<>()
-        when:
-        EventLoopGroup workerGroup = new NioEventLoopGroup()
+    @Test
+    fun `HTTP pipelining is supported correctly`() {
+
+        val responses = ArrayList<HttpResponse>()
+
+        val workerGroup = NioEventLoopGroup()
+
         try {
-            Bootstrap b = new Bootstrap()
+            val b = Bootstrap()
             b.group(workerGroup)
-            b.channel(NioSocketChannel.class)
-                    b.handler(new ChannelInitializer<SocketChannel>() {
-                @Override
-                void initChannel(SocketChannel ch) throws Exception {
-                    ch.pipeline().addLast(new HttpClientCodec())
-                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
-                        @Override
-                        void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                        if (msg instanceof HttpResponse) {
-                            responses.add(msg)
+            b.channel(NioSocketChannel::class.java)
+            b.handler(object : ChannelInitializer<SocketChannel>() {
+                override fun initChannel(ch: SocketChannel) {
+                    ch.pipeline().addLast(HttpClientCodec())
+                    ch.pipeline().addLast(object : ChannelInboundHandlerAdapter() {
+                        override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
+                            if (msg is HttpResponse) {
+                                responses.add(msg)
+                            }
+                            super.channelRead(ctx, msg)
+                            if (responses.size == 3) {
+                                ctx.channel().close()
+                            }
                         }
-                        super.channelRead(ctx, msg)
-                        if (responses.size() == 3) {
-                            ctx.channel().close()
-                        }
-                    }
                     })
                 }
             })
 
-                    ChannelFuture f = b.connect("localhost", 9999).sync()
-                    f.channel().writeAndFlush(new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/pipelining/1000"))
+            val f = b.connect("localhost", 9999).sync()
+            f.channel().writeAndFlush(DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/pipelining/1000"))
             f.channel().writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT)
-            f.channel().writeAndFlush(new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/pipelining/500"))
+            f.channel().writeAndFlush(DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/pipelining/500"))
             f.channel().writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT)
-            f.channel().writeAndFlush(new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/pipelining/10"))
+            f.channel().writeAndFlush(DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/pipelining/10"))
             f.channel().writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT)
 
             // Wait until the connection is closed.
             f.channel().closeFuture().sync()
+
         } finally {
             workerGroup.shutdownGracefully()
         }
-        then:
-        responses.size() == 3
-        responses.get(0).headers().get("URI") == "/pipelining/1000"
-        responses.get(1).headers().get("URI") == "/pipelining/500"
-        responses.get(2).headers().get("URI") == "/pipelining/10"
+
+        assertEquals(3, responses.size)
+        assertEquals("/pipelining/1000", responses[0].headers().get("URI"))
+        assertEquals("/pipelining/500", responses[1].headers().get("URI"))
+        assertEquals("/pipelining/10", responses[2].headers().get("URI"))
     }
 
-    def "async JSON calls work"() {
-        given:
-        def uri = "/test/json/async"
-        def expectedHeaders = ['content-type': 'application/json;charset=UTF-8']
-        when:
-        def data = WebServerSpec.callAndRead(uri, null, expectedHeaders)
-        then:
-        Json.parseObject(data).get("test").asText() == '1'
+    @Test
+    fun `async JSON calls work`() {
+
+        val uri = "/test/json/async"
+        val expectedHeaders = mapOf("content-type" to "application/json;charset=UTF-8")
+
+        val data = WebServerSpec.callAndRead(uri, null, expectedHeaders)
+
+        assertEquals("1", Json.parseObject(data).get("test").asText())
     }
 
 }
