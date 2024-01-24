422b68e4854e854f1f44c0981610d1737c3be796
Sascha Bieberstein <sbi@scireum.de>
2024-01-24 18:33:22 +0100

    Adapts test to kotlin syntax

--- src/test/kotlin/sirius/pasta/noodle/CompilationContextTest.kt
+++ src/test/kotlin/sirius/pasta/noodle/CompilationContextTest.kt
@@ -8,18 +8,25 @@
 
 package sirius.pasta.noodle
 
-class CompilationContextTest extends BaseSpecification {
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.kernel.SiriusExtension
+import sirius.kernel.tokenizer.Position
+import sirius.pasta.noodle.compiler.CompilationContext
+import kotlin.test.assertEquals
 
-    def "resolving a classes works"() {
-        when:
-        CompilationContext compilationContext = new CompilationContext()
-        compilationContext.addImport(Position.UNKNOWN,  "System", System.class);
-        then: "Resolving fully qualified classes works"
-        compilationContext.tryResolveClass("java.lang.String").get() == String.class
-        and: "Aliases work"
-        compilationContext.tryResolveClass("String").get() == String.class
-        and: "Imports work"
-        compilationContext.tryResolveClass("System").get() == System.class
+@ExtendWith(SiriusExtension::class)
+class CompilationContextTest {
+
+    @Test
+    fun `resolving a classes works`() {
+        val compilationContext = CompilationContext(null)
+        compilationContext.addImport(Position.UNKNOWN, "System", System::class.java)
+        // Resolving fully qualified classes works
+        assertEquals(String::class.java, compilationContext.tryResolveClass("java.lang.String").get())
+        // Aliases work
+        assertEquals(String::class.java, compilationContext.tryResolveClass("String").get())
+        // Imports work
+        assertEquals(System::class.java, compilationContext.tryResolveClass("System").get())
     }
-
 }
