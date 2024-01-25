e3f56c30c2a80be26d69b5c7fec4413ad0f9b193
Sascha Bieberstein <sbi@scireum.de>
2024-01-25 11:56:32 +0100

    Adapts tests to kotlin syntax

--- src/test/kotlin/sirius/pasta/tagliatelle/LocalScopeTest.kt
+++ src/test/kotlin/sirius/pasta/tagliatelle/LocalScopeTest.kt
@@ -8,168 +8,208 @@
 
 package sirius.pasta.tagliatelle
 
-import sirius.kernel.tokenizer.ParseError
-import sirius.kernel.BaseSpecification
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
+import sirius.kernel.tokenizer.ParseError
 import sirius.pasta.noodle.compiler.CompileError
 import sirius.pasta.noodle.compiler.CompileException
 import sirius.pasta.tagliatelle.compiler.TemplateCompiler
 import sirius.web.resources.Resources
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-class LocalScopeTest extends BaseSpecification {
+/**
+ * Tests different local scope scenarios in templates.
+ */
+@ExtendWith(SiriusExtension::class)
+class LocalScopeTest {
 
-    @Part
-    private static Tagliatelle tagliatelle
-
-            @Part
-            private static Resources resources
-
-            @Part
-            private static TestHelper test
-
-            def "locals within loops cleanup works"() {
-        given:
-        List<String> list = ["a", "b", "c"]
-        String expectedResult = resources.resolve("templates/local-scope.html").get().getContentAsString()
-        when:
-        def ctx = tagliatelle.
-        createResourceCompilationContext("test",
+    @Test
+    fun `Locals within loops cleanup works`() {
+        val list = listOf("a", "b", "c")
+        val expectedResult = resources.resolve("templates/local-scope.html").get().contentAsString
+        val context = tagliatelle.createResourceCompilationContext(
+                "test",
                 resources.resolve("/templates/local-scope.html.pasta").get(),
-                null)
-        List<CompileError> errors = new TemplateCompiler(ctx).compile()
-        then:
-        errors.size() == 0
-        and:
-        test.basicallyEqual(ctx.getTemplate().renderToString(list), expectedResult)
+                null
+        )
+        val errors = TemplateCompiler(context).compile()
+
+        assertTrue { errors.isEmpty() }
+        assertTrue { test.basicallyEqual(context.template.renderToString(list), expectedResult) }
     }
 
-    def "failing access out of scope for loops and blocks works"() {
-        given:
-        List<String> list = ["a", "b", "c"]
-        List<CompileError> errors
-                when:
+    @Test
+    fun `Failing access out of scope for loops and blocks works`() {
+        val errors = mutableListOf<CompileError>()
+
         try {
-            def ctx = tagliatelle.
-            createResourceCompilationContext("test",
+            val context = tagliatelle.createResourceCompilationContext(
+                    "test",
                     resources.resolve("/templates/out-of-scope.html.pasta").get(),
-                    null)
-            new TemplateCompiler(ctx).compile()
-        } catch (CompileException err) {
-            errors = err.getErrors()
+                    null
+            )
+            TemplateCompiler(context).compile()
+        } catch (exception: CompileException) {
+            errors.addAll(exception.errors)
         }
-        then:
-        errors.size() == 5
-        errors.get(0).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(0).toString().contains("Unknown variable test")
-        errors.get(1).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(1).toString().contains("Unknown variable el")
-        errors.get(2).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(2).toString().contains("Unknown variable test")
-        errors.get(3).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(3).toString().contains("Unknown variable el")
-        errors.get(4).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(4).toString().contains("Unknown variable test")
+
+        assertEquals(5, errors.size)
+        errors[0].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable test") }
+        }
+        errors[1].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable el") }
+        }
+        errors[2].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable test") }
+        }
+        errors[3].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable el") }
+        }
+        errors[4].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable test") }
+        }
     }
 
-    def "failing access out of scope for if works"() {
-        when:
-        List<CompileError> errors
-                try {
-                    def source = "<i:if test=\"1 == 1\"><i:local name=\"test\" value=\"1\"/></i:if>@test " +
+    @Test
+    fun `Failing access out of scope for if works`() {
+        val errors = mutableListOf<CompileError>()
+        try {
+            val source = "<i:if test=\"1 == 1\"><i:local name=\"test\" value=\"1\"/></i:if>@test " +
                     "@if(1 == 1){<i:local name=\"test\" value=\"1\"/>} @test"
-                    def ctx = tagliatelle.createInlineCompilationContext("test", source, null)
-                    new TemplateCompiler(ctx,).compile()
-                } catch (CompileException err) {
-                    errors = err.getErrors()
-                }
-        then:
-        errors.size() == 2
-        errors.get(0).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(0).toString().contains("Unknown variable test")
-        errors.get(1).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(1).toString().contains("Unknown variable test")
+            val context = tagliatelle.createInlineCompilationContext("test", source, null)
+            TemplateCompiler(context).compile()
+        } catch (exception: CompileException) {
+            errors.addAll(exception.errors)
+        }
+
+        assertEquals(2, errors.size)
+        errors[0].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable test") }
+        }
+        errors[1].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable test") }
+        }
     }
 
-    def "failing access out of scope for else works"() {
-        when:
-        List<CompileError> errors
-                try {
-                    def source = "<i:if test=\"1 == 1\"><i:else><i:local name=\"test\" value=\"1\"/></i:else></i:if>@test"
-                    def ctx = tagliatelle.createInlineCompilationContext("test", source, null)
-                    new TemplateCompiler(ctx).compile()
-                } catch (CompileException err) {
-                    errors = err.getErrors()
-                }
-        then:
-        errors.size() == 1
-        errors.get(0).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(0).toString().contains("Unknown variable test")
+    @Test
+    fun `Failing access out of scope for else works`() {
+        val errors = mutableListOf<CompileError>()
+        try {
+            val source =
+                    "<i:if test=\"1 == 1\"><i:else><i:local name=\"test\" value=\"1\"/></i:else></i:if>@test"
+            val context = tagliatelle.createInlineCompilationContext("test", source, null)
+            TemplateCompiler(context).compile()
+        } catch (exception: CompileException) {
+            errors.addAll(exception.errors)
+        }
+
+        assertEquals(1, errors.size)
+        errors[0].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable test") }
+        }
     }
 
-    def "failing access out of scope for render works"() {
-        when:
-        List<CompileError> errors
-                try {
-                    def source = "<e:scope>@outer</e:scope>"
-                    def ctx = tagliatelle.createInlineCompilationContext("test", source, null)
-                    new TemplateCompiler(ctx,).compile()
-                } catch (CompileException err) {
-                    errors = err.getErrors()
-                }
-        then:
-        errors.size() == 1
-        errors.get(0).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(0).toString().contains("Unknown variable outer")
+    @Test
+    fun `Failing access out of scope for render works`() {
+        val errors = mutableListOf<CompileError>()
+        try {
+            val source = "<e:scope>@outer</e:scope>"
+            val context = tagliatelle.createInlineCompilationContext("test", source, null)
+            TemplateCompiler(context).compile()
+        } catch (exception: CompileException) {
+            errors.addAll(exception.errors)
+        }
+
+        assertEquals(1, errors.size)
+        errors[0].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable outer") }
+        }
     }
 
-    def "failing access out of scope for invoke works"() {
-        when:
-        List<CompileError> errors
-                try {
-                    def source = "<i:invoke template=\"/templates/invoke-scope.html.pasta\"/>@invokeLocal"
-                    def ctx = tagliatelle.createInlineCompilationContext("test", source, null)
-                    new TemplateCompiler(ctx).compile()
-                } catch (CompileException err) {
-                    errors = err.getErrors()
-                }
-        then:
-        errors.size() == 1
-        errors.get(0).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(0).toString().contains("Unknown variable invokeLocal")
+    @Test
+    fun `Failing access out of scope for invoke works`() {
+        val errors = mutableListOf<CompileError>()
+        try {
+            val source = "<i:invoke template=\"/templates/invoke-scope.html.pasta\"/>@invokeLocal"
+            val context = tagliatelle.createInlineCompilationContext("test", source, null)
+            TemplateCompiler(context).compile()
+        } catch (exception: CompileException) {
+            errors.addAll(exception.errors)
+        }
+
+        assertEquals(1, errors.size)
+        errors[0].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable invokeLocal") }
+        }
     }
 
-    def "failing access out of scope for include works"() {
-        when:
-        List<CompileError> errors
-                try {
-                    def source = "<i:include name=\"/templates/invoke-scope.html.pasta\"/>@invokeLocal"
-                    def ctx = tagliatelle.createInlineCompilationContext("test", source, null)
-                    new TemplateCompiler(ctx).compile()
-                } catch (CompileException err) {
-                    errors = err.getErrors()
-                }
-        then:
-        errors.size() == 2
-        errors.get(1).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(1).toString().contains("Unknown variable invokeLocal")
+    @Test
+    fun `Failing access out of scope for include works`() {
+        val errors = mutableListOf<CompileError>()
+        try {
+            val source = "<i:include name=\"/templates/invoke-scope.html.pasta\"/>@invokeLocal"
+            val context = tagliatelle.createInlineCompilationContext("test", source, null)
+            TemplateCompiler(context).compile()
+        } catch (exception: CompileException) {
+            errors.addAll(exception.errors)
+        }
+
+        assertEquals(2, errors.size)
+        errors[1].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable invokeLocal") }
+        }
     }
 
-    def "failing access out of scope for if/else works"() {
-        when:
-        List<CompileError> errors
-                try {
-                    def source = "<i:if test=\"true\"><i:local name=\"test\" value=\"1\"/><i:else>@test</i:else></i:if>" +
-                    "@if (true){<i:local name=\"test\" value=\"1\"/>}else{@test}"
-                    def ctx = tagliatelle.createInlineCompilationContext("test", source, null)
-                    new TemplateCompiler(ctx).compile()
-                } catch (CompileException err) {
-                    errors = err.getErrors()
-                }
-        then:
-        errors.size() == 2
-        errors.get(0).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(0).toString().contains("Unknown variable test")
-        errors.get(1).getError().getSeverity() == ParseError.Severity.ERROR
-        errors.get(1).toString().contains("Unknown variable test")
+    @Test
+    fun `Failing access out of scope for if else works`() {
+        val errors = mutableListOf<CompileError>()
+        try {
+            val source =
+                    "<i:if test=\"true\"><i:local name=\"test\" value=\"1\"/><i:else>@test</i:else></i:if>" +
+                            "@if (true){<i:local name=\"test\" value=\"1\"/>}else{@test}"
+            val context = tagliatelle.createInlineCompilationContext("test", source, null)
+            TemplateCompiler(context).compile()
+        } catch (exception: CompileException) {
+            errors.addAll(exception.errors)
+        }
+
+        assertEquals(2, errors.size)
+        errors[0].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable test") }
+        }
+        errors[1].apply {
+            assertEquals(ParseError.Severity.ERROR, error.severity)
+            assertTrue { toString().contains("Unknown variable test") }
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        @Part
+        private lateinit var tagliatelle: Tagliatelle
+
+        @JvmStatic
+        @Part
+        private lateinit var resources: Resources
+
+        @JvmStatic
+        @Part
+        private lateinit var test: TestHelper
     }
 }
