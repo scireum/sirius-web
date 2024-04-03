cde293278a0457597c7096f52e55b7ec57e12178
Andreas Haufler <aha@scireum.de>
2024-04-04 00:05:48 +0200

    Removes compilation warnings for tests

--- src/test/kotlin/sirius/web/http/CSRFTokenTest.kt
+++ src/test/kotlin/sirius/web/http/CSRFTokenTest.kt
@@ -15,8 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import sirius.kernel.SiriusExtension
 import sirius.kernel.commons.Streams
 import java.net.HttpURLConnection
-import java.net.URL
-
+import java.net.URI
 import java.nio.charset.StandardCharsets
 import kotlin.test.assertEquals
 
@@ -34,8 +33,9 @@ class CSRFTokenTest {
     @Test
     fun `safePOST() works if the token is present via GET`() {
 
-        val connection = URL("http://localhost:9999/test/provide-security-token").openConnection() as HttpURLConnection
-        connection.setRequestMethod("GET")
+        val connection =
+                URI("http://localhost:9999/test/provide-security-token").toURL().openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
         connection.connect()
         val token = String(Streams.toByteArray(connection.inputStream), StandardCharsets.UTF_8)
 
@@ -63,64 +63,68 @@ class CSRFTokenTest {
     @Test
     fun `safePOST() works if correct token is present via POST`() {
 
-        val connection = URL("http://localhost:9999/test/provide-security-token").openConnection() as HttpURLConnection
-        connection.setRequestMethod("GET")
+        val connection =
+                URI("http://localhost:9999/test/provide-security-token").toURL().openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
         connection.connect()
         val token = String(Streams.toByteArray(connection.inputStream), StandardCharsets.UTF_8)
 
 
-        val connection2 = URL(
-            "http://localhost:9999/test/fake-delete-data?CSRFToken=$token"
-        ).openConnection() as HttpURLConnection
-        connection2.setRequestMethod("POST")
+        val connection2 = URI(
+                "http://localhost:9999/test/fake-delete-data?CSRFToken=$token"
+        ).toURL().openConnection() as HttpURLConnection
+        connection2.requestMethod = "POST"
         connection2.setRequestProperty(
-            HttpHeaderNames.COOKIE.toString(),
-            connection.headerFields["set-cookie"]?.get(0)
+                HttpHeaderNames.COOKIE.toString(),
+                connection.headerFields["set-cookie"]?.get(0)
         )
         connection2.connect()
 
-        assertEquals(200, connection2.getResponseCode())
+        assertEquals(200, connection2.responseCode)
     }
 
     @Test
     fun `safePOST() success on POST if expired token is provided`() {
 
-        val connection = URL("http://localhost:9999/test/provide-security-token").openConnection() as HttpURLConnection
-        connection.setRequestMethod("GET")
+        val connection =
+                URI("http://localhost:9999/test/provide-security-token").toURL().openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
         connection.connect()
         val token = String(Streams.toByteArray(connection.inputStream), StandardCharsets.UTF_8)
         TestRequest.GET("/test/expire-security-token").execute()
 
-        val connection2 = URL(
-            "http://localhost:9999/test/fake-delete-data?CSRFToken=$token"
-        ).openConnection() as HttpURLConnection
-        connection2.setRequestMethod("POST")
+        val connection2 = URI(
+                "http://localhost:9999/test/fake-delete-data?CSRFToken=$token"
+        ).toURL().openConnection() as HttpURLConnection
+        connection2.requestMethod = "POST"
         connection2.setRequestProperty(
-            HttpHeaderNames.COOKIE.toString(),
-            connection.headerFields["set-cookie"]?.get(0)
+                HttpHeaderNames.COOKIE.toString(),
+                connection.headerFields["set-cookie"]?.get(0)
         )
         connection2.connect()
 
-        assertEquals(200,connection2.getResponseCode())
+        assertEquals(200, connection2.responseCode)
     }
 
     @Test
     fun `safePOST() works if false token is given via POST`() {
 
-        val connection = URL("http://localhost:9999/test/provide-security-token").openConnection() as HttpURLConnection
-        connection.setRequestMethod("GET")
+        val connection =
+                URI("http://localhost:9999/test/provide-security-token").toURL().openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
         connection.connect()
 
         val connection2 =
-            URL("http://localhost:9999/test/fake-delete-data?CSRFToken=w-r-o-n-g-t-o-k-e-n").openConnection() as HttpURLConnection
-        connection2.setRequestMethod("POST")
+                URI("http://localhost:9999/test/fake-delete-data?CSRFToken=w-r-o-n-g-t-o-k-e-n").toURL()
+                        .openConnection() as HttpURLConnection
+        connection2.requestMethod = "POST"
         connection2.setRequestProperty(
-            HttpHeaderNames.COOKIE.toString(),
-            connection.headerFields["set-cookie"]?.get(0)
+                HttpHeaderNames.COOKIE.toString(),
+                connection.headerFields["set-cookie"]?.get(0)
         )
         connection2.connect()
 
-        assertEquals(500,connection2.getResponseCode())
+        assertEquals(500, connection2.responseCode)
     }
 
     @Test
@@ -128,7 +132,7 @@ class CSRFTokenTest {
 
         val result = TestRequest.POST("/test/fake-delete-data-unsafe").execute()
 
-        assertEquals(HttpResponseStatus.OK,result.status)
+        assertEquals(HttpResponseStatus.OK, result.status)
     }
 
     @Test
@@ -136,7 +140,7 @@ class CSRFTokenTest {
 
         val result = TestRequest.GET("/test/fake-delete-data-unsafe").execute()
 
-        assertEquals(HttpResponseStatus.INTERNAL_SERVER_ERROR,result.status)
+        assertEquals(HttpResponseStatus.INTERNAL_SERVER_ERROR, result.status)
     }
 
     @Test
@@ -150,32 +154,34 @@ class CSRFTokenTest {
     @Test
     fun `ensureSafePOST() goes wrong on POST if false token is given`() {
 
-        val connection = URL(
-            "http://localhost:9999/test/fake-delete-data-ensure-safe?CSRFToken=w-r-o-n-g-t-o-k-e-n"
-        ).openConnection() as HttpURLConnection
-        connection.setRequestMethod("POST")
+        val connection = URI(
+                "http://localhost:9999/test/fake-delete-data-ensure-safe?CSRFToken=w-r-o-n-g-t-o-k-e-n"
+        ).toURL().openConnection() as HttpURLConnection
+        connection.requestMethod = "POST"
         connection.connect()
 
-        assertEquals(401,connection.responseCode )
+        assertEquals(401, connection.responseCode)
     }
 
     @Test
     fun `ensureSafePOST() works as intended if correct token is given via POST`() {
 
-        val connection = URL("http://localhost:9999/test/provide-security-token").openConnection() as HttpURLConnection
-        connection.setRequestMethod("GET")
+        val connection =
+                URI("http://localhost:9999/test/provide-security-token").toURL().openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
         connection.connect()
-        val token = String(Streams.toByteArray(connection.getInputStream()), StandardCharsets.UTF_8)
+        val token = String(Streams.toByteArray(connection.inputStream), StandardCharsets.UTF_8)
 
         val connection2 =
-            URL("http://localhost:9999/test/fake-delete-data-ensure-safe?CSRFToken=$token").openConnection() as HttpURLConnection
-        connection2.setRequestMethod("POST")
+                URI("http://localhost:9999/test/fake-delete-data-ensure-safe?CSRFToken=$token").toURL()
+                        .openConnection() as HttpURLConnection
+        connection2.requestMethod = "POST"
         connection2.setRequestProperty(
-            HttpHeaderNames.COOKIE.toString(),
-            connection.headerFields["set-cookie"]?.get(0)
+                HttpHeaderNames.COOKIE.toString(),
+                connection.headerFields["set-cookie"]?.get(0)
         )
         connection2.connect()
 
-        assertEquals(200,connection2.getResponseCode())
+        assertEquals(200, connection2.getResponseCode())
     }
 }
