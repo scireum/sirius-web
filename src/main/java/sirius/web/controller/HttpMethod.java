8656f47f5b2995057c8742ddfd0fbd53c2d46dca
Jakob Vogel <jvo@scireum.de>
2025-10-25 19:21:57 +0200

    Adds helper to check whether a list of HTTP methods is complete 🔍
    …that we can use primarily for debug output. In such cases, we only want to highlight subsets of methods. Always printing the full default list may be hiding important information.
    
    SIRI-1141

--- src/main/java/sirius/web/controller/HttpMethod.java
+++ src/main/java/sirius/web/controller/HttpMethod.java
@@ -8,6 +8,9 @@
 
 package sirius.web.controller;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 /**
  * Enumerates the HTTP methods.
  * <p>
@@ -18,11 +21,29 @@ public enum HttpMethod {
     CONNECT, DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT, TRACE;
 
     /**
+     * Holds all possible HTTP methods for quick reference.
+     */
+    private static final Set<HttpMethod> ALL_METHODS = EnumSet.allOf(HttpMethod.class);
+
+    /**
      * Converts this enum to the corresponding Netty {@link io.netty.handler.codec.http.HttpMethod HttpMethod}.
      *
      * @return the corresponding Netty HTTP method
      */
     public io.netty.handler.codec.http.HttpMethod toHttpMethod() {
         return io.netty.handler.codec.http.HttpMethod.valueOf(this.name());
+    }
+
+    /**
+     * Determines if the given array of methods contains all possible HTTP methods.
+     *
+     * @param methods the list of methods to check
+     * @return <tt>true</tt> if all HTTP methods are contained, <tt>false</tt> otherwise
+     */
+    public static boolean isCompleteList(HttpMethod... methods) {
+        if (methods == null || methods.length == 0 || methods.length < ALL_METHODS.size()) {
+            return false;
+        }
+        return EnumSet.of(methods[0], methods).containsAll(ALL_METHODS);
     }
 }
