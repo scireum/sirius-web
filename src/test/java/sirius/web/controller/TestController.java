38940cb9a24782a22a1bbbe736e382b38eddc232
Jakob Vogel <jvo@scireum.de>
2025-10-25 16:47:45 +0200

    Reworks test controller 👷
    …to use proper variable names, to list parts towards the top, and to include a documentation comment.
    
    SIRI-1141

--- src/test/java/sirius/web/controller/TestController.java
+++ src/test/java/sirius/web/controller/TestController.java
@@ -34,6 +34,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.zip.GZIPInputStream;
 
+/**
+ * Provides various test endpoints used by the unit and integration tests.
+ */
 @Register
 public class TestController extends BasicController {
 
@@ -43,266 +46,277 @@ public class TestController extends BasicController {
     @Part
     private CSRFHelper csrfHelper;
 
+    @Part
+    private Resources resources;
+
     @Override
     public void onError(WebContext webContext, HandledException error) {
         webContext.respondWith().error(HttpResponseStatus.INTERNAL_SERVER_ERROR, error.getMessage());
     }
 
     @Routed("/test/post")
-    public void postTest(WebContext ctx) {
-        ctx.respondWith()
-           .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/plain")
-           .direct(HttpResponseStatus.OK, ctx.get("value").asString());
+    public void postTest(WebContext webContext) {
+        webContext.respondWith()
+                  .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/plain")
+                  .direct(HttpResponseStatus.OK, webContext.get("value").asString());
     }
 
     @InternalService
     @Routed("/test/json")
-    public void testJSON(WebContext ctx, JSONStructuredOutput out) {
-        out.property("test", ctx.getParameter("test"));
+    public void testJSON(WebContext webContext, JSONStructuredOutput out) {
+        out.property("test", webContext.getParameter("test"));
     }
 
     @Routed("/rewrite")
-    public void testReqriting(WebContext ctx) {
-        ctx.respondWith().direct(HttpResponseStatus.OK, "OK");
+    public void testRewriting(WebContext webContext) {
+        webContext.respondWith().direct(HttpResponseStatus.OK, "OK");
     }
 
     @Routed("/test/cookieCacheTest")
-    public void testCookieCacheTest(WebContext ctx) {
-        ctx.setCookie("Test", "1", 3600, CookieHeaderNames.SameSite.Strict, WebContext.CookieSecurity.IF_SSL);
-        ctx.respondWith().cached().direct(HttpResponseStatus.OK, "OK");
+    public void testCookieCacheTest(WebContext webContext) {
+        webContext.setCookie("Test", "1", 3600, CookieHeaderNames.SameSite.Strict, WebContext.CookieSecurity.IF_SSL);
+        webContext.respondWith().cached().direct(HttpResponseStatus.OK, "OK");
     }
 
     @InternalService
     @Routed("/test/json-param/:1")
-    public void testJSONParam(WebContext ctx, JSONStructuredOutput out, String param) {
-        out.property("test", param);
+    public void testJSONParam(WebContext webContext, JSONStructuredOutput output, String parameter) {
+        output.property("test", parameter);
     }
 
     @InternalService
     @Routed("/test/json-params/:1/:2")
-    public void testJSONParams(WebContext ctx, JSONStructuredOutput out, String param1, String param2) {
-        out.property("param1", param1);
-        out.property("param2", param2);
+    public void testJSONParams(WebContext webContext,
+                               JSONStructuredOutput output,
+                               String parameter1,
+                               String parameter2) {
+        output.property("param1", parameter1);
+        output.property("param2", parameter2);
     }
 
     @InternalService
     @Routed("/test/mixed-json-params/:2/:1")
-    public void testMixedJSONParams(WebContext ctx, JSONStructuredOutput out, String param1, String param2) {
-        out.property("param1", param1);
-        out.property("param2", param2);
+    public void testMixedJSONParams(WebContext webContext,
+                                    JSONStructuredOutput output,
+                                    String parameter1,
+                                    String parameter2) {
+        output.property("param1", parameter1);
+        output.property("param2", parameter2);
     }
 
     @InternalService
     @Routed("/test/json-params-varargs/:1/:2/**")
-    public void testJSONWithVarArgs(WebContext ctx,
-                                    JSONStructuredOutput out,
-                                    String param1,
-                                    String param2,
-                                    List<String> paramList) {
-        out.property("param1", param1);
-        out.property("param2", param2);
-        out.array("params", "param", paramList);
+    public void testJSONWithVarArgs(WebContext webContext,
+                                    JSONStructuredOutput output,
+                                    String parameter1,
+                                    String parameter2,
+                                    List<String> parameterList) {
+        output.property("param1", parameter1);
+        output.property("param2", parameter2);
+        output.array("params", "param", parameterList);
     }
 
     @Routed("/tunnel/test")
-    public void tunnelTest(WebContext ctx) {
-        ctx.respondWith().setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test").tunnel("http://localhost:9999/api/test");
+    public void tunnelTest(WebContext webContext) {
+        webContext.respondWith()
+                  .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test")
+                  .tunnel("http://localhost:9999/api/test");
     }
 
     @Routed("/tunnel/test/tune")
-    public void tunnelTuneTest(WebContext ctx) {
-        ctx.respondWith()
-           .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test")
-           .tunnel("http://localhost:9999/tunnel/test/tune/target", request -> request.setMethod("POST"), null, null);
+    public void tunnelTuneTest(WebContext webContext) {
+        webContext.respondWith()
+                  .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test")
+                  .tunnel("http://localhost:9999/tunnel/test/tune/target",
+                          request -> request.setMethod("POST"),
+                          null,
+                          null);
     }
 
     @Routed("/tunnel/test/tune/target")
-    public void tunnelTuneTestTarget(WebContext ctx) {
-        ctx.respondWith().direct(HttpResponseStatus.OK, ctx.getRequest().method().name());
+    public void tunnelTuneTestTarget(WebContext webContext) {
+        webContext.respondWith().direct(HttpResponseStatus.OK, webContext.getRequest().method().name());
     }
 
     @Routed("/tunnel/fallback_for_404")
-    public void tunnelFallbackFor404(WebContext ctx) {
-        ctx.respondWith()
-           .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test")
-           .tunnel("http://localhost:9999/api/DOES_NOT_EXIST",
-                   (code) -> ctx.respondWith()
-                                .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test")
-                                .tunnel("http://localhost:9999/api/test"));
+    public void tunnelFallbackFor404(WebContext webContext) {
+        webContext.respondWith()
+                  .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test")
+                  .tunnel("http://localhost:9999/api/DOES_NOT_EXIST",
+                          code -> webContext.respondWith()
+                                            .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test")
+                                            .tunnel("http://localhost:9999/api/test"));
     }
 
     @Routed("/tunnel/fallback_for_error")
-    public void tunnelFallbackForError(WebContext ctx) {
-        ctx.respondWith()
-           .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test")
-           .tunnel("http://localhost:1",
-                   (code) -> ctx.respondWith()
-                                .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test")
-                                .tunnel("http://localhost:9999/api/test"));
+    public void tunnelFallbackForError(WebContext webContext) {
+        webContext.respondWith()
+                  .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test")
+                  .tunnel("http://localhost:1",
+                          code -> webContext.respondWith()
+                                            .setHeader(HttpHeaderNames.CONTENT_TYPE, "text/test")
+                                            .tunnel("http://localhost:9999/api/test"));
     }
 
     @Routed("/tunnel/test_large")
-    public void tunnelTestLarge(WebContext ctx) {
-        ctx.respondWith().tunnel("http://localhost:9999/api/test/test_large");
+    public void tunnelTestLarge(WebContext webContext) {
+        webContext.respondWith().tunnel("http://localhost:9999/api/test/test_large");
     }
 
     @Routed("/tunnel/test_transform")
-    public void tunnelTestTransform(WebContext ctx) {
-        ctx.respondWith().tunnel("http://localhost:9999/api/test/test_large", buf -> {
-            if (buf.readableBytes() == 0) {
+    public void tunnelTestTransform(WebContext webContext) {
+        webContext.respondWith().tunnel("http://localhost:9999/api/test/test_large", buffer -> {
+            if (buffer.readableBytes() == 0) {
                 return Optional.empty();
             }
 
-            byte[] array = buf.array();
+            byte[] array = buffer.array();
             // Transform all bytes by + 1
             for (int i = 0; i < array.length; i++) {
                 array[i] = (byte) ((array[i] + 1) % 255);
             }
 
-            buf.retain();
-            return Optional.of(buf);
+            buffer.retain();
+            return Optional.of(buffer);
         }, null);
     }
 
     @Routed(value = "/test/predispatch", preDispatchable = true)
-    public void testPredispatch(WebContext ctx, InputStreamHandler in) throws Exception {
+    public void testPredispatch(WebContext webContext, InputStreamHandler input) throws Exception {
         int size = 0;
-        while (in.read() >= 0) {
+        while (input.read() >= 0) {
             size++;
         }
-        in.close();
-        ctx.respondWith().direct(HttpResponseStatus.OK, String.valueOf(size));
+        input.close();
+        webContext.respondWith().direct(HttpResponseStatus.OK, String.valueOf(size));
     }
 
     @Routed(value = "/test/predispatch/abort", preDispatchable = true)
-    public void testPredispatchAbort(WebContext ctx, InputStreamHandler in) throws Exception {
-        ctx.respondWith().direct(HttpResponseStatus.OK, "ABORT");
+    public void testPredispatchAbort(WebContext webContext, InputStreamHandler input) {
+        webContext.respondWith().direct(HttpResponseStatus.OK, "ABORT");
     }
 
     @Routed("/test/os")
-    public void testOutputStream(WebContext ctx) throws IOException {
-        OutputStream out = ctx.respondWith().outputStream(HttpResponseStatus.OK, "text/plain");
+    public void testOutputStream(WebContext webContext) throws IOException {
+        OutputStream output = webContext.respondWith().outputStream(HttpResponseStatus.OK, "text/plain");
         byte[] buffer = new byte[8192];
         for (int i = 0; i < 9; i++) {
-            out.write(buffer, 0, 8192);
+            output.write(buffer, 0, 8192);
         }
-        out.close();
+        output.close();
     }
 
-    @Part
-    private Resources res;
-
     @Routed("/test/resource")
-    public void testResource(WebContext ctx) throws IOException {
-        ctx.respondWith().resource(res.resolve("assets/test_large.css").get().getUrl().openConnection());
+    public void testResource(WebContext webContext) throws IOException {
+        webContext.respondWith().resource(resources.resolve("assets/test_large.css").get().getUrl().openConnection());
     }
 
     @Routed("/test/resource_uncompressable")
-    public void testResourceUncompressable(WebContext ctx) throws IOException {
-        ctx.respondWith()
-           .named("test_large.jpg")
-           .resource(res.resolve("assets/test_large.css").get().getUrl().openConnection());
+    public void testResourceUncompressable(WebContext webContext) throws IOException {
+        webContext.respondWith()
+                  .named("test_large.jpg")
+                  .resource(resources.resolve("assets/test_large.css").get().getUrl().openConnection());
     }
 
     @InternalService
     @Routed(value = "/upload-test", preDispatchable = true)
-    public void uploadTest(WebContext ctx, JSONStructuredOutput out, InputStreamHandler upload) throws IOException {
-        try {
+    public void uploadTest(WebContext webContext, JSONStructuredOutput output, InputStreamHandler upload)
+            throws IOException {
+        try (upload) {
             long size = Streams.exhaust(upload);
-            out.property("size", size);
-        } finally {
-            upload.close();
+            output.property("size", size);
         }
     }
 
     @InternalService
     @Routed(value = "/upload-gzip", preDispatchable = true)
-    public void uploadGzipTest(WebContext ctx, JSONStructuredOutput out, InputStreamHandler upload) throws IOException {
+    public void uploadGzipTest(WebContext webContext, JSONStructuredOutput output, InputStreamHandler upload)
+            throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new GZIPInputStream(upload)))) {
-            out.property("lines", reader.lines().count());
+            output.property("lines", reader.lines().count());
         }
     }
 
     @Routed("/test/firewall")
     @Limited
-    public void firewallTest(WebContext ctx) {
-        ctx.respondWith().direct(HttpResponseStatus.OK, "OK");
+    public void firewallTest(WebContext webContext) {
+        webContext.respondWith().direct(HttpResponseStatus.OK, "OK");
     }
 
     @Routed("/test/firewallBlocked")
     @Limited("blocked")
-    public void firewallTestBlocked(WebContext ctx) {
-        ctx.respondWith().direct(HttpResponseStatus.OK, "OK");
+    public void firewallTestBlocked(WebContext webContext) {
+        webContext.respondWith().direct(HttpResponseStatus.OK, "OK");
     }
 
     @Routed("/test/fake-delete-data")
-    public void deleteData(WebContext ctx) {
-        if (!ctx.isSafePOST()) {
-            ctx.respondWith().status(HttpResponseStatus.INTERNAL_SERVER_ERROR);
+    public void deleteData(WebContext webContext) {
+        if (!webContext.isSafePOST()) {
+            webContext.respondWith().status(HttpResponseStatus.INTERNAL_SERVER_ERROR);
             return;
         }
-        ctx.respondWith().status(HttpResponseStatus.OK);
+        webContext.respondWith().status(HttpResponseStatus.OK);
     }
 
     @Routed("/test/fake-delete-data-unsafe")
-    public void deleteDataUnsafe(WebContext ctx) {
-        if (!ctx.isUnsafePOST()) {
-            ctx.respondWith().status(HttpResponseStatus.INTERNAL_SERVER_ERROR);
+    public void deleteDataUnsafe(WebContext webContext) {
+        if (!webContext.isUnsafePOST()) {
+            webContext.respondWith().status(HttpResponseStatus.INTERNAL_SERVER_ERROR);
             return;
         }
-        ctx.respondWith().status(HttpResponseStatus.OK);
+        webContext.respondWith().status(HttpResponseStatus.OK);
     }
 
     @Routed("/test/fake-delete-data-ensure-safe")
-    public void deleteDataEnsureSafe(WebContext ctx) {
+    public void deleteDataEnsureSafe(WebContext webContext) {
         try {
-            if (!ctx.ensureSafePOST()) {
-                ctx.respondWith().status(HttpResponseStatus.INTERNAL_SERVER_ERROR);
+            if (!webContext.ensureSafePOST()) {
+                webContext.respondWith().status(HttpResponseStatus.INTERNAL_SERVER_ERROR);
                 return;
             }
-        } catch (HandledException e) {
-            ctx.respondWith().status(HttpResponseStatus.UNAUTHORIZED);
+        } catch (HandledException _) {
+            webContext.respondWith().status(HttpResponseStatus.UNAUTHORIZED);
             return;
         }
 
-        ctx.respondWith().status(HttpResponseStatus.OK);
+        webContext.respondWith().status(HttpResponseStatus.OK);
     }
 
     @Routed("/test/provide-security-token")
-    public void provideSecurityToken(WebContext ctx) {
-        ctx.respondWith().template("/templates/security-token.html.pasta");
+    public void provideSecurityToken(WebContext webContext) {
+        webContext.respondWith().template("/templates/security-token.html.pasta");
     }
 
     @Routed("/test/expire-security-token")
-    public void expireSecurityToken(WebContext ctx) {
-        csrfHelper.recomputeCSRFToken(ctx);
-        ctx.respondWith().direct(HttpResponseStatus.OK, "OK");
+    public void expireSecurityToken(WebContext webContext) {
+        csrfHelper.recomputeCSRFToken(webContext);
+        webContext.respondWith().direct(HttpResponseStatus.OK, "OK");
     }
 
     @InternalService
     @Routed("/test/json/async")
-    public Future asyncJSONCall(WebContext ctx, JSONStructuredOutput out) {
+    public Future asyncJSONCall(WebContext webContext, JSONStructuredOutput output) {
         return tasks.defaultExecutor().start(() -> {
             Wait.seconds(1);
-            out.property("test", "1");
+            output.property("test", "1");
         });
     }
 
     @Routed("/test/session-test")
-    public void sessionTest(WebContext ctx) {
-        ctx.setSessionValue("test1", "test");
-        ctx.setSessionValue("test2", null);
+    public void sessionTest(WebContext webContext) {
+        webContext.setSessionValue("test1", "test");
+        webContext.setSessionValue("test2", null);
 
-        ctx.respondWith().direct(HttpResponseStatus.OK, "OK");
+        webContext.respondWith().direct(HttpResponseStatus.OK, "OK");
     }
 
     @Routed("/test/redirect-to-get")
-    public void redirect(WebContext ctx) {
-        ctx.respondWith().redirectToGet("/test/redirect-target");
+    public void redirect(WebContext webContext) {
+        webContext.respondWith().redirectToGet("/test/redirect-target");
     }
 
     @Routed("/test/redirect-target")
-    public void redirectTarget(WebContext ctx) {
-        ctx.respondWith().direct(HttpResponseStatus.OK, "target");
+    public void redirectTarget(WebContext webContext) {
+        webContext.respondWith().direct(HttpResponseStatus.OK, "target");
     }
 }
