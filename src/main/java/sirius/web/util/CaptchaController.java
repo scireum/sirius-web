00836dd5c56248e1bc4796e39e59f114b186bb2c
Yannick Möller <ymo@scireum.de>
2026-01-15 14:40:38 +0100

    Introduce a simple taglib for a captcha powered by altcha
    altcha does not use any cookies or DSGVO-problematic approaches and is quiet simple.
    the open-source version of altcha is licensed under MIT-license - details see https://github.com/altcha-org/altcha
    
    a captcha can be simply added in e.g. public forms to protect against spam/Bot input by
    adding the t:captcha to the form + adding the verify-method to the formController-Route,
    as it is "only" spam protection the hmac-key must not be that complex and can be the same for the whole system.
    
    - fixes: SE-14997

---
+++ src/main/java/sirius/web/util/CaptchaController.java
@@ -0,0 +1,88 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Stuttgart, Germany
+ *
+ * Copyright by scireum GmbH
+ * https://www.scireum.de - info@scireum.de
+ */
+
+package sirius.web.util;
+
+import com.google.common.io.BaseEncoding;
+import org.altcha.altcha.Altcha;
+import sirius.kernel.commons.Strings;
+import sirius.kernel.di.std.Register;
+import sirius.kernel.health.Exceptions;
+import sirius.web.controller.BasicController;
+import sirius.web.controller.Controller;
+import sirius.web.controller.Routed;
+import sirius.web.http.WebContext;
+import sirius.web.services.InternalService;
+import sirius.web.services.JSONStructuredOutput;
+
+import java.security.SecureRandom;
+
+/**
+ * Provides captcha related functionality for bot/spam-protection in forms.
+ * This may be helpful for public forms like contact or registration forms.
+ * For more information, see <a href="https://github.com/altcha-org/altcha">Altcha on GitHub</a>.
+ * To use the captcha, a challenge is requested via {@link #captchaChallenge(WebContext, JSONStructuredOutput)} by the
+ * Taglib t:captcha. To verify the solution, {@link #verifyCaptcha(WebContext)} must be
+ * implemented in the form-submit Route to make protection work properly.
+ *
+ */
+@Register(classes = {CaptchaController.class, Controller.class})
+public class CaptchaController extends BasicController {
+
+    private static final String CAPTCHA_FAILED = "CaptchaController.captchaFailed";
+
+    private static final String CAPTCHA_CHALLENGE_HMAC_KEY =
+            BaseEncoding.base32().encode(new SecureRandom().generateSeed(8));
+
+    /**
+     * Provides a captcha challenge for form submissions.
+     * Called by the Taglib t:captcha to obtain a new challenge.
+     *
+     * @param ctx the current request
+     * @param out JSON output for writing the challenge to the request
+     */
+    @InternalService
+    @Routed("/captcha-challenge")
+    public void captchaChallenge(WebContext ctx, JSONStructuredOutput out) {
+        try {
+            Altcha.ChallengeOptions options = new Altcha.ChallengeOptions();
+            options.hmacKey = CAPTCHA_CHALLENGE_HMAC_KEY;
+
+            Altcha.Challenge challenge = Altcha.createChallenge(options);
+            out.property("algorithm", challenge.algorithm);
+            out.property("challenge", challenge.challenge);
+            out.property("maxnumber", challenge.maxnumber);
+            out.property("salt", challenge.salt);
+            out.property("signature", challenge.signature);
+        } catch (Exception _) {
+            throw Exceptions.createHandled().withNLSKey(CAPTCHA_FAILED).handle();
+        }
+    }
+
+    /**
+     * Verifies the captcha solution contained in the given context.
+     * This method should be called in form-submit routes to verify the captcha solution.
+     *
+     * @param ctx the web context containing the captcha solution
+     */
+    public void verifyCaptcha(WebContext ctx) {
+        try {
+            String payload = ctx.get("altcha").asString();
+            if (Strings.isEmpty(payload)) {
+                throw Exceptions.createHandled().withNLSKey(CAPTCHA_FAILED).handle();
+            }
+
+            boolean isValid = Altcha.verifySolution(payload, CAPTCHA_CHALLENGE_HMAC_KEY, true);
+            if (!isValid) {
+                throw Exceptions.createHandled().withNLSKey(CAPTCHA_FAILED).handle();
+            }
+        } catch (Exception _) {
+            throw Exceptions.createHandled().withNLSKey(CAPTCHA_FAILED).handle();
+        }
+    }
+}
