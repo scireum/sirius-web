9ae72595731ac2c1b1d2593a6b641cb23ec3e33e
Jakob Vogel <jvo@scireum.de>
2026-02-14 03:55:09 +0100

    Revises API info to handle sections 🥌
    Services without a section are sorted into an undecorated default section, thus maintaining legacy behaviour.
    
    SIRI-1167

--- src/main/java/sirius/web/services/PublicApiInfo.java
+++ src/main/java/sirius/web/services/PublicApiInfo.java
@@ -8,11 +8,17 @@
 
 package sirius.web.services;
 
+import sirius.kernel.commons.Strings;
 import sirius.kernel.settings.Extension;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * Provides a description of a public web API.
@@ -50,25 +56,55 @@ import java.util.List;
 public class PublicApiInfo {
 
     private final String apiName;
+
     private final Extension extension;
-    private final List<PublicServiceInfo> services = new ArrayList<>();
+
+    /**
+     * The default section to which all services without a {@linkplain PublicService#apiSection() section string} are
+     * added. It has no name, is always sorted first, and is presented without any decorations in the UI, equivalent to
+     * the legacy implementation before the introduction of sections.
+     */
+    private final PublicApiSectionInfo defaultSection = new PublicApiSectionInfo(this, null);
+
+    /**
+     * Contains all sections of this API, including the default section. The sections are sorted by their priority and
+     * then by their name, so the default section is always first, followed by configured sections, and then by
+     * non-configured sections.
+     */
+    private final SortedSet<PublicApiSectionInfo> sections;
+
+    /**
+     * Contains all known sections by their name, so we can easily look them up when adding services.
+     * <p>
+     * Note that the default section is not contained in this map, as it has no name and is always available via the
+     * {@link #defaultSection} field.
+     */
+    private final Map<String, PublicApiSectionInfo> knownSectionsByName = new HashMap<>();
 
     protected PublicApiInfo(String apiName, Extension extension) {
         this.apiName = apiName;
         this.extension = extension;
+
+        this.sections = new TreeSet<>(Comparator.comparing(PublicApiSectionInfo::getPriority)
+                                                .thenComparing(PublicApiSectionInfo::getName));
     }
 
     protected void addService(PublicServiceInfo serviceInfo) {
-        synchronized (services) {
-            services.add(serviceInfo);
-            services.sort(Comparator.comparing(PublicServiceInfo::getPriority)
-                                    .thenComparing(PublicServiceInfo::getLabel));
+        synchronized (sections) {
+            PublicApiSectionInfo section =
+                    Optional.ofNullable(serviceInfo.getApiSection()).filter(Strings::isFilled).map(sectionName -> {
+                        return knownSectionsByName.computeIfAbsent(sectionName,
+                                                                   _ -> new PublicApiSectionInfo(this, sectionName));
+                    }).orElse(defaultSection);
+
+            section.addService(serviceInfo);
+            sections.add(section);
         }
     }
 
-    public List<PublicServiceInfo> getServices() {
-        synchronized (services) {
-            return new ArrayList<>(services);
+    public List<PublicApiSectionInfo> getSections() {
+        synchronized (sections) {
+            return new ArrayList<>(sections);
         }
     }
 
