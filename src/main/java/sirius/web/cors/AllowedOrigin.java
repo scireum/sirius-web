2a270e3884f5ba09b9699fd55bc5c92383e08277
Hauke Sommerfeld <hso@scireum.de>
2026-08-05 16:14:31 +0200

    Make CORS credentials an explicit opt-in via `AllowedOrigin.Specific`
    Adds an `allowCredentials` flag to `AllowedOrigin.Specific` so that
    `Access-Control-Allow-Credentials` is only ever sent for an explicit
    allow-list of origins. `MatchRequest` and `Wildcard` never allow
    credentials, avoiding the reflect-any-origin-with-credentials attack
    vector.
    
    SIRI-1241

--- src/main/java/sirius/web/cors/AllowedOrigin.java
+++ src/main/java/sirius/web/cors/AllowedOrigin.java
@@ -28,28 +28,45 @@ public sealed interface AllowedOrigin
 
     /**
      * The {@code Access-Control-Allow-Origin} header should be set to the origin of the request.
+     * <p>
+     * Note that for security reasons the {@code Access-Control-Allow-Credentials} header will <b>not be set</b>  in
+     * this case. Reflecting the request's {@code Origin} while allowing credentials behaves essentially like a wildcard
+     * with allowed credentials - which is forbidden and opens an attack vector for cross-site data theft.
+     * </p>
      */
     record MatchRequest() implements AllowedOrigin {
     }
 
     /**
      * The {@code Access-Control-Allow-Origin} header should be set to the origin of the request
-     * <b>if it is in the provided collection of allowed origins</b>.
+     * if it is in the provided collection of allowed origins.
      * <p>
      * If it is not, the server should respond accordingly: e.g. by <i>not</i> setting the header at all, resulting in
      * a client-side CORS error.
      * </p>
+     * <p>
+     * Note that the {@code Access-Control-Allow-Credentials: true} header will be sent {@code true} if
+     * {@code allowCredentials} is set to {@code true}.
+     * By default, a value of {@code false} should be preferred, unless the specified origins explicitly need to send
+     * cookies.
+     * </p>
      *
-     * @param origins the allowed origins
+     * @param allowCredentials if set to {@code true}, the {@code Access-Control-Allow-Credentials} header will be set
+     *                         to {@code true}
+     * @param origins          the allowed origins
      */
-    record Specific(Collection<String> origins) implements AllowedOrigin {
-        public Specific(String... origins) {
-            this(Arrays.stream(origins).collect(Collectors.toSet()));
+    record Specific(boolean allowCredentials, Collection<String> origins) implements AllowedOrigin {
+        public Specific(boolean allowCredentials, String... origins) {
+            this(allowCredentials, Arrays.stream(origins).collect(Collectors.toSet()));
         }
     }
 
     /**
      * The {@code Access-Control-Allow-Origin} header should be set to allow all origins (wildcard).
+     * <p>
+     * Note that the {@code Access-Control-Allow-Credentials} header will <b>not be set</b>  in this case for security
+     * reasons (the combination if wildcard and allowed credentials is explicitly forbidden by the CORS standard).
+     * </p>
      */
     record Wildcard() implements AllowedOrigin {
     }
